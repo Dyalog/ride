42f61c825e02c1e9c85892ab63a130d8609f9327
ngn <nick.nickolov@gmail.com>
2014-10-07 10:03:15 +0100

    re-generated self-signed certificate

--- ssl/generate.sh
+++ ssl/generate.sh
@@ -2,6 +2,6 @@
 set -e
 
 openssl genrsa -out key.pem 1024
-openssl req -new -key key.pem -out certrequest.csr
+openssl req -new -days 100 -key key.pem -out certrequest.csr
 openssl x509 -req -in certrequest.csr -signkey key.pem -out cert.pem
 rm certrequest.csr
