9d6fb296863d930115199ecfb6757500a7cb4c90
ngn <nick.nickolov@gmail.com>
2014-11-03 11:23:28 +0000

    adjustment to the node-webkit packaging process

--- dist.sh
+++ dist.sh
@@ -2,6 +2,9 @@
 set -e
 ./build.coffee
 node node_modules/node-webkit-builder/bin/nwbuild -p win,osx,linux32,linux64 -v 0.10.5 -o dist static
+
+# https://github.com/rogerwang/node-webkit/wiki/The-solution-of-lacking-libudev.so.0
 echo 'Fixing dependency udev.so.0 → udev.so.1'
 for f in dist/dyalog/linux{32,64}/dyalog; do sed -i 's/udev\.so\.0/udev.so.1/g' $f; done
+
 echo 'Done.'
