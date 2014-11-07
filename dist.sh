0b7dda7b038de184c7ebe679db6fdf86840ad345
ngn <nick.nickolov@gmail.com>
2014-11-07 14:14:20 +0000

    run nwbuild with its --quiet option

--- dist.sh
+++ dist.sh
@@ -5,7 +5,7 @@ coffee=node_modules/coffee-script/bin/coffee
 $coffee ./build.coffee
 $coffee -c server.coffee && mv server.js static/
 
-node node_modules/node-webkit-builder/bin/nwbuild -p win,osx,linux32,linux64 -v 0.10.5 -o dist static
+node node_modules/node-webkit-builder/bin/nwbuild --quiet -p win,osx,linux32,linux64 -v 0.10.5 -o dist static
 
 # https://github.com/rogerwang/node-webkit/wiki/The-solution-of-lacking-libudev.so.0
 echo 'Fixing dependency udev.so.0 → udev.so.1'
