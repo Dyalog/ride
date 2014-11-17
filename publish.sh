74c3b70aa7ebf905672155d67e93c2755d21c89a
ngn <nick.nickolov@gmail.com>
2014-11-17 10:45:43 +0000

    scripted publishing releases on /devt

---
+++ publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+if [ `id -un` != nick -a "$1" != iknow ]; then echo "here be dragons; don't run it unless you know what you're doing"; exit 1; fi
+git stash && ./clean.sh && ./dist.sh && git stash pop
+if ! mountpoint /devt >/dev/null; then echo 'mounting devt' && sudo mount /devt; fi
+r=/devt/ride/jsride
+d=$r/`date +%Y-%m-%d--%H-%M`
+if [ -e $d ]; then echo "$d already exists, try again later"; exit 2; fi
+echo "copying to $d" && cp -r build/dyalogjs $d
+echo 'all available releases on /devt:' && ls $r
