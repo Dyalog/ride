e9cd3893ef2be3b8287454ff90a2b8c6c8f43084
ngn <nick.nickolov@gmail.com>
2014-12-10 15:42:54 +0000

    pruned unnecessary help files

--- docs/update.sh
+++ docs/update.sh
@@ -6,11 +6,8 @@ echo 'Exporting doc archive from svn...'
 z=webhelp.zip
 svn export --force -q http://svn.dyalog.bramley/svn/docbin/trunk/documentation/web/$z
 echo 'Extracting...'
-rm -rf tmp help && unzip -q $z -d tmp && mv tmp/Content help && rm -rf tmp $z
+rm -rf Content && unzip -q $z 'Content/Language/*' && rm -rf help $z && mv Content help
 echo 'Clearing executable flag from files...'
 find help -type f -print0 | xargs -0 chmod a-x
-echo 'Removing some unnecessary files...'
-rm -rf help/Glossary.* help/SkinSupport/ help/Resources/ \
-    help/InterfaceGuide/Images/Grid_Components.png
 ../node_modules/coffee-script/bin/coffee ./fatburner.coffee
 echo 'Done.'
