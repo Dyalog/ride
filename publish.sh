9966a78128184bbac51e442b597045c760207bb3
ngn <nick.nickolov@gmail.com>
2015-01-07 13:51:35 +0000

    rewrote publish.sh so Jenkins can run it

--- publish.sh
+++ publish.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+# This script is for Jenkins (continuous integration) to run.
 set -e
-if [ `id -un` != nick -a "$1" != iknow ]; then echo "here be dragons; don't run it unless you know what you're doing"; exit 1; fi
-git stash && ./clean.sh && ./dist.sh && (git stash pop || true)
-if ! mountpoint /devt >/dev/null; then echo 'mounting devt' && sudo mount /devt; fi
+umask 002 # user and group can do everything, others can only read and execute
+mountpoint /devt; echo good # make sure it's mounted
 r=/devt/ride/jsride
-d=`date +%Y-%m-%d--%H-%M`
-if [ -e $r/$d ]; then echo "$r/$d already exists, try again later"; exit 2; fi
-echo "copying to $r/$d" && sudo cp -r build/dyalogjs $r/$d
-echo 'updating "latest" symlink'
-[ -L $r/latest ] && sudo rm $r/latest && sudo ln -s $d $r/latest
-echo "all available releases in $r:" && ls $r
+d=`date +%Y-%m-%d--%H-%M` # append a letter to $d if such a directory already exists
+for suffix in '' {a..z}; do if [ ! -e $r/$d$suffix ]; then d=$d$suffix; break; fi; done
+echo "copying to $r/$d"; cp -r build/dyalogjs $r/$d
+echo 'updating "latest" symlink'; l=$r/latest; [ -L $l ]; rm $l; ln -s $d $l
+echo 'cleaning up old releases'
+for x in $(ls $r | grep -P '^\d{4}-\d{2}-\d{2}--\d{2}-\d{2}[a-z]?$' | sort | head -n-10); do
+  echo "deleting $x"; rm -rf $r/$x || true
+done
