5a81fbdb4239466e59a9cb07c9bd8af65ebc56d2
ngn <nick.nickolov@gmail.com>
2015-01-14 16:12:35 +0000

    reduced size of nw executable by a few kB
    omitted unused font files

--- dist.sh
+++ dist.sh
@@ -7,14 +7,17 @@ node_version=0.11.4
 
 ulimit -n $(ulimit -Hn) # Bump open file limit to its hard limit.  OSX build requires a lot.
 
-if [ proxy.coffee -nt build/static/proxy.js ]; then
-  echo 'compiling proxy.coffee'
-  $coffee -o build/static/ -c proxy.coffee
-fi
+b=build/tmp/nwb
+echo 'copying static files to a clean temp directory for node-webkit-build'
+rm -rf $b; cp -r build/static $b
+echo 'compiling proxy.coffee'
+$coffee -o $b -c proxy.coffee
+echo 'removing extra font formats'
+rm $b/apl385.{eot,svg,ttf}
 
 desktop_app() {
   echo "building desktop app for $1"
-  node node_modules/node-webkit-builder/bin/nwbuild --quiet -p $1 -v $node_version -o build build/static
+  node node_modules/node-webkit-builder/bin/nwbuild --quiet -p $1 -v $node_version -o build $b
 }
 for platform in ${@:-win osx linux}; do desktop_app $platform; done
 
