e1952b11e842f82ccea72b1ebf7a9b3f79f2d18e
ngn <nick.nickolov@gmail.com>
2015-02-17 11:16:15 +0000

    improved release script

--- GH-Release.sh
+++ GH-Release.sh
@@ -2,49 +2,48 @@
 set -e
 
 # create JSON
-JSONTMP='/tmp/GH-Publish.json'
-BASE_VERSION=0.1
-COMMIT_NUMBER=`git rev-list HEAD --count`
-VERSION="${BASE_VERSION}.${COMMIT_NUMBER}"
+TMP_JSON=/tmp/GH-Publish.json
+BASE_VERSION=`npm show ride version` # parse "version" field from package.json, returned with an extra ".0"
+VERSION="${BASE_VERSION%%.0}.`git rev-list HEAD --count`"  # "%%.0" strips trailing ".0"
 
 if [ -s GHTOKEN ]; then
   TOKEN="`cat GHTOKEN`"
 else
-  echo "Please put your Github API Token a file named \"GHTOKEN\""
+  echo 'Please put your GitHub API Token in a file named "GHTOKEN"'
   exit 1
 fi
-cat >${JSONTMP} <<-!!EOF
+cat >$TMP_JSON <<.
 {
-  "tag_name": "v${VERSION}",
+  "tag_name": "v$VERSION",
   "target_commitish": "master",
-  "name": "v${VERSION}",
-  "body": "Pre-Release of Ride 2.0",
+  "name": "v$VERSION",
+  "body": "Pre-release of RIDE 2.0",
   "draft": false,
   "prerelease": true
 }
-!!EOF
+.
 
-curl -o /tmp/GH-Response.json --data @${JSONTMP} -H "Authorization: token ${TOKEN}" -i https://api.github.com/repos/Dyalog/RideJS/releases
+REPO=Dyalog/ride # ideally this should be parsed from "git ls-remote --get-url origin"
+TMP_RESPONSE=/tmp/GH-Response.json
+curl -o $TMP_RESPONSE --data @$TMP_JSON -H "Authorization: token $TOKEN" -i https://api.github.com/repos/$REPO/releases
 
-ReleaseID=`cat /tmp/GH-Response.json | grep "\"id\"" | head -1 | sed 's/.*: //;s/,//'`
+RELEASE_ID=`grep '"id"' $TMP_RESPONSE | head -1 | sed 's/.*: //;s/,//'`
 
-echo "Created release with Id: ${ReleaseID}"
+echo "created release with id: $RELEASE_ID"
 
 for DIR in `ls build/ride`; do
-
-  cd build/ride/${DIR}
-  echo "creating /tmp/Ride-${VERSION}-${DIR}.zip"
-  zip -q -r /tmp/Ride-${VERSION}-${DIR}.zip .
-
-  echo "uploading File..."
-  curl -o /dev/null -H "Authorization: token ${TOKEN}" \
-    -H "Accept: application/vnd.github.manifold-preview" \
-    -H "Content-Type: application/zip" \
-    --data-binary @/tmp/Ride-${VERSION}-${DIR}.zip \
-    "https://uploads.github.com/repos/Dyalog/RideJS/releases/${ReleaseID}/assets?name=Ride-${VERSION}-${DIR}.zip"
-
-  rm /tmp/Ride-${VERSION}-${DIR}.zip
+  ZIP=ride-${VERSION}-${DIR}.zip
+  TMP_ZIP=/tmp/$ZIP
+  cd build/ride/$DIR
+  echo "creating $TMP_ZIP"
+  zip -q -r "$TMP_ZIP" .
+  echo 'uploading'
+  curl -o /dev/null -H "Authorization: token $TOKEN" \
+    -H 'Accept: application/vnd.github.manifold-preview' \
+    -H 'Content-Type: application/zip' \
+    --data-binary @"$TMP_ZIP" \
+    https://uploads.github.com/repos/$REPO/releases/$RELEASE_ID/assets?name=$ZIP
+  rm "$TMP_ZIP"
   cd -
 done
-rm /tmp/GH-Response.json
-rm $JSONTMP
+rm $TMP_RESPONSE $TMP_JSON
