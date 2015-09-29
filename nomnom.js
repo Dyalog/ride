42640e843c77ab4dd2d14489200a9dfaa214f777
ngn <nick.nickolov@gmail.com>
2015-09-29 15:30:44 +0100

    improved usage string in nomnom

--- nomnom.js
+++ nomnom.js
@@ -6,9 +6,11 @@ this.options=function(h){return{parse:function(a){ // h:option descriptions, a:a
   for(var k in h){r[k]=h[k]['default'];h[k].abbr&&(abbr[h[k].abbr]=k)}
   function usage(){
     var s='Usage: '+a[0]+' [OPTION]...\n\n'
-    for(var k in h)
-      s+='  '+(h[k].abbr?'-'+h[k].abbr+',':'   ')+' --'+k+Array(Math.max(2,17-k.length)).join(' ')
-        +(h[k].help||'')+(h[k]['default']==null?'':'  ['+h[k]['default']+']')+'\n'
+    for(var k in h){
+      var mv=h[k].metavar?' '+h[k].metavar:''
+      s+='  '+(h[k].abbr?'-'+h[k].abbr+mv+',':'   ')+' --'+k+mv+Array(Math.max(2,16-(k+mv).length)).join(' ')
+        +(h[k].help||'')+(h[k].flag||h[k]['default']==null?'':'  ['+h[k]['default']+']')+'\n'
+    }
     process.stderr.write(s)
   }
   function err(s){process.stderr.write(s+'\n\n');usage();process.exit(1)}
