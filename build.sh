4936c23827f18d3c98713c6ac982b38ff16610be
ngn <nick.nickolov@gmail.com>
2015-10-02 13:34:33 +0100

    got rid of pure-cjs
    In 25 loc I can do what it does in 300+
    
    Side effect: D.js lost 100k

--- build.sh
+++ build.sh
@@ -69,10 +69,29 @@ if [ ! -e build/nw/D.js -o $(find build/{js,tmp} -newer build/nw/D.js 2>/dev/nul
 .
   echo 'combining js files into one'
   node >build/tmp/ride.js <<.
-    require('pure-cjs').transform({input:'build/js/client/init.js',dryRun:1}).then(
-      function(h){process.stdout.write(h.code)}, // success
-      function(e){process.stderr.write(e);process.exit(1)} // failure
-    );
+    // Combine a bunch of CommonJS modules into a single js file,
+    // similar to Browserify (http://browserify.org/) or pure-cjs (https://github.com/RReverser/pure-cjs)
+    var path=require('path'),fs=require('fs')
+    function combine(ps,p0){return(
+      ';(function(){\n'+
+      'var m={\n'+
+        ps.map(function(p){return(
+          JSON.stringify(p)+':{_:function(module,require){\n'+fs.readFileSync(p,'utf8')+';return module.exports}}'
+        )}).join(',\n')+'\n'+
+      '}\n'+
+      'function load(k){\n'+
+      '  if(!m[k])throw Error("no module named "+JSON.stringify(k))\n'+
+      '  if(m[k]._){m[k]=m[k]._.apply(m[k],[{exports:m[k]},function(x){\n'+
+      '    x=k.split("/").slice(0,-1).concat(x.split("/"))\n'+
+      '    var i=0;while(i<x.length)x[i]==="."?x.splice(i,1):x[i]===".."?x.splice(--i,2):i++\n'+
+      '    return load(x.join("/")+".js")\n'+
+      '  }]);delete m[k]._}\n'+
+      '  return m[k]\n'+
+      '}\n'+
+      'load('+JSON.stringify(p0)+')\n'+
+      '}());\n'
+    )}
+    process.stdout.write(combine(fs.readdirSync('client').map(function(x){return'client/'+x}),'client/init.js'))
 .
   echo 'generating D.js for desktop app'
   cat build/tmp/version-info.js build/tmp/libs.js init-nw.js build/tmp/ride.js >build/nw/D.js
