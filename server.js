fee2dd5ffbc473071163bd61ae1972fd6fbcafed
ngn <nick.nickolov@gmail.com>
2015-10-09 11:15:30 +0100

    use env vars instead of cmd line args in server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+
+// Environment variables:
+//   DYALOG_IDE_SERVER_WATCH=1     watch for changes and rebuild automatically (useful for development)
+//   DYALOG_IDE_SERVER_INSECURE=1  use http (on port 8000) instead of https (on port 8443)
+//   DYALOG_IDE_SERVER_IPV6=1      use IPv6
+
 'use strict'
 var compression=require('compression'),express=require('express'),fs=require('fs'),http=require('http'),
     https=require('https'),nomnom=require('./nomnom'),WSServer=require('ws').Server,
@@ -7,15 +13,7 @@ var compression=require('compression'),express=require('express'),fs=require('fs
 var t0=+new Date
 function log(s){process.stdout.write((new Date-t0)+': '+s+'\n')}
 
-var opts=nomnom.options({
-  cert:{metavar:'FILE',help:'PEM-encoded certificate for https','default':'ssl/cert.pem'},
-  key:{metavar:'FILE',help:'PEM-encoded private key for https','default':'ssl/key.pem'},
-  insecure:{flag:true,help:'use http (on port 8000) instead of https (on port 8443)'},
-  ipv6:{abbr:'6',flag:true,help:'use IPv6'},
-  watch:{abbr:'w',flag:true,help:'watch for changes and rebuild (useful for development)'}
-}).parse()
-
-if(opts.watch){
+if(process.env.DYALOG_IDE_SERVER_WATCH){
   var _d=__dirname, tid, isRunning
   function build(){
     if(isRunning){
@@ -25,9 +23,10 @@ if(opts.watch){
       spawn(_d+'/build.sh',{cwd:_d,stdio:'inherit'}).on('close',function(){log('build done');isRunning=0})
     }
   }
-  ;['.','client','style','style/themes'].forEach(function(x){
+  ;['.','client','style','style/img','style/themes'].forEach(function(x){
     fs.watch(_d+'/'+x,function(){tid=tid||setTimeout(build,100)})
   })
+  build()
 }
 
 var app=express()
@@ -35,9 +34,10 @@ app.disable('x-powered-by')
 app.use(function(req,res,next){log(req.method+' '+req.path);next()})
 app.use(compression())
 app.use('/',express.static('build/static'))
-var port=opts.insecure?8000:8443
-var server=opts.insecure?http.createServer(app)
-                        :https.createServer({cert:fs.readFileSync(opts.cert),key:fs.readFileSync(opts.key)},app)
+var insecure=process.env.DYALOG_IDE_SERVER_INSECURE
+var port=insecure?8000:8443
+var server=insecure?http.createServer(app)
+             :https.createServer({cert:fs.readFileSync('ssl/cert.pem'),key:fs.readFileSync('ssl/key.pem')},app)
 var proxy=Proxy()
 ;(new WSServer({server:server})).on('connection',function(ws){
   var l={},io={} // l:listeners, io:socket.io-like interface
@@ -47,4 +47,4 @@ var proxy=Proxy()
   ws.on('message',function(m){io.onevent({data:JSON.parse(m)})})
   proxy(io)
 })
-server.listen(port,opts.ipv6?'::':'0.0.0.0',function(){log('listening on port '+port)})
+server.listen(port,process.env.DYALOG_IDE_SERVER_IPV6?'::':'0.0.0.0',function(){log('server started on port '+port)})
