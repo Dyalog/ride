7fcf900d4b303b2f89abf2bfb43b4380f16f3560
ngn <nick.nickolov@gmail.com>
2015-11-03 10:25:45 +0000

    shorter and faster code in prefs-code.js
    bypass jQuery where we can

--- client/prefs-code.js
+++ client/prefs-code.js
@@ -1,9 +1,9 @@
 'use strict'
-var prefs=require('./prefs')
-var $ai,$sw,$aim,$icom,$swm,$io,$mb,$acbr,$acbl,$acbe,$ac,$acd,$fold
+var prefs=require('./prefs'),on=CodeMirror.on
+var h={} // various input elements
 this.name='Code'
 this.init=function($e){
-  $e.html(
+  $e[0].innerHTML=
     '<div>'+
       '<p><label><input id=code-ai   type=checkbox>Auto-indent</label> <label><input id=code-sw  size=1> spaces</label>'+
       '<p><label><input id=code-aim  type=checkbox>in methods:</label> <label><input id=code-swm size=1> spaces</label>'+
@@ -16,58 +16,48 @@ this.init=function($e){
       '<p><label><input id=code-ac   type=checkbox>Autocompletion</label> <label>after <input id=code-acd size=5>ms</label>'+
       '<p><label><input id=code-fold type=checkbox>Code folding (outlining)</label>'+
     '</div>'
-  )
-  $ai  =$('#code-ai'  )
-  $sw  =$('#code-sw'  )
-  $aim =$('#code-aim' )
-  $swm =$('#code-swm' )
-  $icom=$('#code-icom')
-  $io  =$('#code-io'  )
-  $mb  =$('#code-mb'  )
-  $acbr=$('#code-acbr')
-  $acbl=$('#code-acbl')
-  $acbe=$('#code-acbe')
-  $ac  =$('#code-ac'  )
-  $acd =$('#code-acd' )
-  $fold=$('#code-fold')
-  $ai.add($aim).change(function(){
-    $sw.add($aim).prop('disabled',!$ai.is(':checked'))
-    $swm.prop('disabled',!$ai.is(':checked')||!$aim.is(':checked'))
+  var a=['ai','sw','aim','swm','icom','io','mb','acbr','acbl','acbe','ac','acd','fold']
+  for(var i=0;i<a.length;i++)h[a[i]]=document.getElementById('code-'+a[i])
+  ;[h.ai,h.aim].forEach(function(x){
+    on(x,'change',function(){
+      h.sw.disabled=h.aim.disabled=!h.ai.checked
+      h.swm.disabled=!h.ai.checked||!h.aim.checked
+    })
   })
-  $acbl.change(function(){$acbe.prop('disabled',!$(this).is(':checked'))})
-  $ac  .change(function(){$acd .prop('disabled',!$(this).is(':checked'))})
-  $sw.add($swm).add($acd).click(function(){$(this).select()})
+  on(h.acbl,'change',function(){h.acbe.disabled=!this.checked})
+  on(h.ac  ,'change',function(){h.acd .disabled=!this.checked})
+  ;[h.sw,h.swm,h.acd].forEach(function(x){on(x,'change',function(){$(this).select()})})
 }
 this.load=function(){
-  var sw =prefs.indent       ();$ai .prop('checked',sw >=0);$sw .val(sw <0&&4||sw )
-  var swm=prefs.indentMethods();$aim.prop('checked',swm>=0);$swm.val(swm<0&&2||swm)
-  $icom.prop('checked',!!prefs.indentComments     ())
-  $io  .prop('checked',!!prefs.indentOnOpen       ())
-  $mb  .prop('checked',!!prefs.matchBrackets      ())
-  $acbr.prop('checked',!!prefs.autoCloseBrackets  ())
-  $acbl.prop('checked',!!prefs.autoCloseBlocks    ())
-  $acbe.val (            prefs.autoCloseBlocksEnd ())
-  $ac  .prop('checked',!!prefs.autocompletion     ())
-  $acd .val (            prefs.autocompletionDelay())
-  $fold.prop('checked',!!prefs.fold               ())
-  $ai.add($acbl).add($ac).change()
+  var sw =prefs.indent       ();h.ai .checked=sw >=0;h.sw .value=sw <0&&4||sw
+  var swm=prefs.indentMethods();h.aim.checked=swm>=0;h.swm.value=swm<0&&2||swm
+  h.icom.checked=!!prefs.indentComments     ()
+  h.io  .checked=!!prefs.indentOnOpen       ()
+  h.mb  .checked=!!prefs.matchBrackets      ()
+  h.acbr.checked=!!prefs.autoCloseBrackets  ()
+  h.acbl.checked=!!prefs.autoCloseBlocks    ()
+  h.acbe.value  =  prefs.autoCloseBlocksEnd ()
+  h.ac  .checked=!!prefs.autocompletion     ()
+  h.acd .value  =  prefs.autocompletionDelay()
+  h.fold.checked=!!prefs.fold               ()
+  $(h.ai).add(h.acbl).add(h.ac).change()
 }
 this.save=function(){
-  prefs.indent             ($ai .is(':checked')?(+$sw .val()||0):-1)
-  prefs.indentMethods      ($aim.is(':checked')?(+$swm.val()||0):-1)
-  prefs.indentComments     ($icom.is(':checked'))
-  prefs.indentOnOpen       ($io  .is(':checked'))
-  prefs.matchBrackets      ($mb  .is(':checked'))
-  prefs.autoCloseBrackets  ($acbr.is(':checked'))
-  prefs.autoCloseBlocks    ($acbl.is(':checked'))
-  prefs.autoCloseBlocksEnd ($acbe.val())
-  prefs.autocompletion     ($ac  .is(':checked'))
-  prefs.autocompletionDelay($acd .val())
-  prefs.fold               ($fold.is(':checked'))
+  prefs.indent             (h.ai .checked?(+h.sw .value||0):-1)
+  prefs.indentMethods      (h.aim.checked?(+h.swm.value||0):-1)
+  prefs.indentComments     (h.icom.checked)
+  prefs.indentOnOpen       (h.io  .checked)
+  prefs.matchBrackets      (h.mb  .checked)
+  prefs.autoCloseBrackets  (h.acbr.checked)
+  prefs.autoCloseBlocks    (h.acbl.checked)
+  prefs.autoCloseBlocksEnd (h.acbe.value)
+  prefs.autocompletion     (h.ac  .checked)
+  prefs.autocompletionDelay(h.acd .value)
+  prefs.fold               (h.fold.checked)
 }
 this.validate=function(){
-  if($ai .is(':checked')&&!isInt($sw .val(),0))return{msg:'Auto-indent must be a non-negative integer.',el:$ai}
-  if($aim.is(':checked')&&!isInt($swm.val(),0))return{msg:'Auto-indent in methods must be a non-negative integer.',el:$aim}
-  if($ac .is(':checked')&&!isInt($acd.val(),1))return{msg:'Autocompletion delay must be a positive integer.',el:$acd}
+  if(h.ai .checked&&!isInt(h.sw .value,0))return{msg:'Auto-indent must be a non-negative integer.'           ,h:h.ai}
+  if(h.aim.checked&&!isInt(h.swm.value,0))return{msg:'Auto-indent in methods must be a non-negative integer.',h:h.aim}
+  if(h.ac .checked&&!isInt(h.acd.value,1))return{msg:'Autocompletion delay must be a positive integer.'      ,h:h.acd}
 }
 function isInt(x,minX){x=+x;return x===(x|0)&&x>=minX}
