9a95a894765ae518f99eace482254a21794dd89b
ngn <nick.nickolov@gmail.com>
2015-12-15 10:59:30 +0000

    treat '[' in addition to '{' as JSON

--- proxy.js
+++ proxy.js
@@ -165,7 +165,7 @@ this.Proxy=function(){
       var n
       while(queue.length>=4&&(n=queue.readInt32BE(0))<=queue.length){
         var m=''+queue.slice(8,n);queue=queue.slice(n);log('from interpreter:'+trunc(JSON.stringify(m)))
-        if(m[0]==='{'){ // let JSON-encoded messages through
+        if(m[0]==='{'||m[0]==='['){ // let JSON-encoded messages through
           log('to browser (JSON):'+trunc(m));socket&&socket.emit(x,y)
         }else if(!/^(?:SupportedProtocols|UsingProtocol)=1$/.test(m)){ // ignore the handshake (SupportedProtocols, etc)
           switch(m.slice(1,m.indexOf('>'))){
