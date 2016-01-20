09d294e3c952fa0a0a80d56b0d76220374951a4b
ngn <nick.nickolov@gmail.com>
2016-01-20 11:08:05 +0000

    some abbreviations

--- proxy.js
+++ proxy.js
@@ -31,47 +31,47 @@ function trunc(s){return s.length>1000?s.slice(0,997)+'...':s}
 function ls(x){return fs.readdirSync(x)}
 function sil(f){return function(x){try{f(x)}catch(_){}}} // exception silencer
 
-var client, // TCP connection to interpreter
-    socket, // websocket or other connection-like object communicating with the browser
-    child,  // a ChildProcess instance, the result from spawn()
-    server  // used to listen for connections from interpreters
+var clt,   // client, TCP connection to interpreter
+    skt,   // websocket or other connection-like object communicating with the browser
+    child, // a ChildProcess instance, the result from spawn()
+    srv    // server, used to listen for connections from interpreters
 function toInterpreter(s){
-  if(client){log('to interpreter:'+trunc(s));var b=Buffer('xxxxRIDE'+s);b.writeInt32BE(b.length,0);client.write(b)}
+  if(clt){log('to interpreter:'+trunc(s));var b=Buffer('xxxxRIDE'+s);b.writeInt32BE(b.length,0);clt.write(b)}
 }
 function cmd(c,h){toInterpreter(JSON.stringify([c,h||{}]))} // c:command name, h:arguments as a JS object
-function toBrowser(x,y){log('to browser:'+trunc(JSON.stringify([x,y])));socket&&socket.emit(x,y)}
-function setUpInterpreterConnection(){
+function toBrowser(x,y){log('to browser:'+trunc(JSON.stringify([x,y])));skt&&skt.emit(x,y)}
+function initInterpreterConn(){
   var q=Buffer(0)
-  client.on('data',function(x){
+  clt.on('data',function(x){
     q=Buffer.concat([q,x]);var n
     while(q.length>=4&&(n=q.readInt32BE(0))<=q.length){
       var m=''+q.slice(8,n);q=q.slice(n);log('from interpreter:'+trunc(m))
-      if(m[0]==='['){log('to browser:'+trunc(m));var u=JSON.parse(m);socket&&socket.emit(u[0],u[1])}
+      if(m[0]==='['){log('to browser:'+trunc(m));var u=JSON.parse(m);skt&&skt.emit(u[0],u[1])}
     }
   })
-  client.on('error',function(e){toBrowser('*connectError',{err:''+e});client=null})
-  client.on('end',function(){log('interpreter diconnected');toBrowser('*disconnected');client=null})
+  clt.on('error',function(e){toBrowser('*connectError',{err:''+e});clt=null})
+  clt.on('end',function(){log('interpreter diconnected');toBrowser('*disconnected');clt=null})
   toInterpreter('SupportedProtocols=1');toInterpreter('UsingProtocol=1')
   cmd('Identify',{identity:1});cmd('Connect',{remoteId:2});cmd('GetWindowLayout')
 }
 var handlers={
   '*connect':function(x){
-    client=net.connect({host:x.host,port:x.port},function(){toBrowser('*connected',{host:x.host,port:x.port})})
-    setUpInterpreterConnection()
+    clt=net.connect({host:x.host,port:x.port},function(){toBrowser('*connected',{host:x.host,port:x.port})})
+    initInterpreterConn()
   },
   '*launch':function(x){
     var exe=(x||{}).exe||process.env.DYALOG_IDE_INTERPRETER_EXE||'dyalog'
-    server=net.createServer(function(c){
-      log('spawned interpreter connected');var a=server.address();server&&server.close();server=null;client=c
-      toBrowser('*connected',{host:a.address,port:a.port});setUpInterpreterConnection()
+    srv=net.createServer(function(c){
+      log('spawned interpreter connected');var a=srv.address();srv&&srv.close();srv=null;clt=c
+      toBrowser('*connected',{host:a.address,port:a.port});initInterpreterConn()
       if(typeof window!=='undefined')window.D.lastSpawnedExe=exe
     })
-    server.on('error',function(err){
+    srv.on('error',function(err){
       log('cannot listen for connections from spawned interpreter: '+err)
-      server=client=null;toBrowser('*listenError',{err:''+err})
+      srv=clt=null;toBrowser('*listenError',{err:''+err})
     })
-    server.listen(0,'127.0.0.1',function(){
-      var a=server.address(),hp=a.address+':'+a.port
+    srv.listen(0,'127.0.0.1',function(){
+      var a=srv.address(),hp=a.address+':'+a.port
       log('listening for connections from spawned interpreter on '+hp)
       log('spawning interpreter '+JSON.stringify(exe))
       var args=['+s','-q'],stdio=['pipe','ignore','ignore']
@@ -84,13 +84,13 @@ var handlers={
       }
       toBrowser('*spawned',{exe:exe,pid:child.pid})
       child.on('error',function(err){
-        server&&server.close();server=client=child=null
+        srv&&srv.close();srv=clt=child=null
         toBrowser('*spawnedError',{
           code:err.code,message:err.code==='ENOENT'?"Cannot find the interpreter's executable":''+err
         })
       })
       child.on('exit',function(code,sig){
-        server&&server.close();server=client=null;toBrowser('*spawnedExited',{code:code,signal:sig});child=null
+        srv&&srv.close();srv=clt=null;toBrowser('*spawnedExited',{code:code,signal:sig});child=null
       })
     })
   },
@@ -113,21 +113,21 @@ var handlers={
       })
     }).on('tcp connection',function(info,accept,reject){
       console.info('incoming connection')
-      client=accept();toBrowser('*connected',{host:'',port:0});setUpInterpreterConnection()
+      clt=accept();toBrowser('*connected',{host:'',port:0});initInterpreterConn()
     }).connect({host:x.host,port:x.port,username:x.user,password:x.pass})
   },
   '*listen':function(x){
-    server=net.createServer(function(c){
+    srv=net.createServer(function(c){
       var t,remoteHost=c&&(t=c.request)&&(t=t.connection)&&t.remoteAddress
-      log('interpreter connected from '+remoteHost);server&&server.close();server=null;client=c
-      toBrowser('*connected',{host:remoteHost,port:x.port});setUpInterpreterConnection()
+      log('interpreter connected from '+remoteHost);srv&&srv.close();srv=null;clt=c
+      toBrowser('*connected',{host:remoteHost,port:x.port});initInterpreterConn()
     })
-    server.on('error',function(err){server=null;toBrowser('*listenError',{err:''+err})})
-    server.listen(x.port,x.host||'',function(){
+    srv.on('error',function(err){srv=null;toBrowser('*listenError',{err:''+err})})
+    srv.listen(x.port,x.host||'',function(){
       log('listening for connections from interpreter on port '+x.port);x.callback&&x.callback()
     })
   },
-  '*listenCancel':function(){server&&server.close()},
+  '*listenCancel':function(){srv&&srv.close()},
   '*getProxyInfo':function(){
     // List available interpreter executables, possible paths are:
     //   C:\Program Files\Dyalog\Dyalog APL $VERSION\dyalog.exe
@@ -139,7 +139,7 @@ var handlers={
     //   /opt/mdyalog/$VERSION/[64|32]/[classic|unicode]/mapl
     //   /Applications/Dyalog-$VERSION/Contents/Resources/Dyalog/mapl
     var interpreters=[]
-    function parseVersion(s){return s.split('.').map(function(x){return+x})}
+    function parseVer(s){return s.split('.').map(function(x){return+x})}
     if(/^win/.test(process.platform)){
       try{
         cp.exec('reg query "HKEY_CURRENT_USER\\Software\\Dyalog" /s /v localdyalogdir',{timeout:2000},
@@ -151,7 +151,7 @@ var handlers={
                 if(m=/^HK.*\\Dyalog APL\/W(-64)? (\d+\.\d+)( Unicode)?$/i.exec(lines[i])){
                   bits=m[1]?64:32;version=m[2];edition=m[3]?'unicode':'classic'
                 }else if(version&&(m=/^ *localdyalogdir +REG_SZ +(\S.*)$/i.exec(lines[i]))){
-                  interpreters.push({exe:m[1]+'dyalog.exe',version:parseVersion(version),bits:bits,edition:edition})
+                  interpreters.push({exe:m[1]+'dyalog.exe',version:parseVer(version),bits:bits,edition:edition})
                 }else if(!/^\s*$/.test(lines[i])){
                   bits=version=edition=null
                 }
@@ -169,7 +169,7 @@ var handlers={
         var a='/Applications',xs=fs.readdirSync(a),m
         for(var i=0;i<xs.length;i++)if(m=/^Dyalog-(\d+\.\d+)\.app$/.exec(xs[i])){
           var exe=a+'/'+xs[i]+'/Contents/Resources/Dyalog/mapl'
-          fs.existsSync(exe)&&interpreters.push({exe:exe,version:parseVersion(m[1]),bits:64,edition:'unicode'})
+          fs.existsSync(exe)&&interpreters.push({exe:exe,version:parseVer(m[1]),bits:64,edition:'unicode'})
         }
       }catch(_){}
       toBrowser('*proxyInfo',{interpreters:interpreters,platform:process.platform})
@@ -180,7 +180,7 @@ var handlers={
           ls(a+'/'+v).forEach(sil(function(b){if(b==='32'||b==='64')
             ls(a+'/'+v+'/'+b).forEach(sil(function(u){if(u==='unicode'||u==='classic'){
               var exe=a+'/'+v+'/'+b+'/'+u+'/mapl'
-              fs.existsSync(exe)&&interpreters.push({exe:exe,version:parseVersion(v),bits:+b,edition:u})
+              fs.existsSync(exe)&&interpreters.push({exe:exe,version:parseVer(v),bits:+b,edition:u})
             }}))
           }))
         }))
@@ -193,7 +193,7 @@ this.Proxy=function(){
   log(new Date().toISOString())
   return function(x){
     log(addr(x)+' connected')
-    ;(socket=x).onevent=function(x){
+    ;(skt=x).onevent=function(x){
       log('from browser:'+trunc(JSON.stringify(x.data)))
       var f=handlers[x.data[0]];f?f(x.data[1]):cmd(x.data[0],x.data[1]||{})
     }
