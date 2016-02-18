1dfa29a64c334b116f313c41826959ede186e274
ngn <nick.nickolov@gmail.com>
2016-02-18 10:41:32 +0000

    ValueTip -> GetValueTip

--- client/vtips.js
+++ client/vtips.js
@@ -5,6 +5,6 @@ this.init=function(w){ // w:window (session or editor)
     .mouseout(function(){clearTimeout(t);t=0})
     .mousemove(function(e){
       var p=w.cm.coordsChar({left:e.clientX,top:e.clientY},'page');clearTimeout(t);t=0;if(p.outside)return
-      t=setTimeout(function(){t=0;w.emit('ValueTip',{win:w.id,line:w.cm.getLine(p.line),pos:p.ch,token:0})},500)
+      t=setTimeout(function(){t=0;w.emit('GetValueTip',{win:w.id,line:w.cm.getLine(p.line),pos:p.ch,token:0})},500)
     })
 }
