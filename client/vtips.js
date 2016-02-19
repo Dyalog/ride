d855b4564e523f4e8d4255732db488abf1de0c04
ngn <nick.nickolov@gmail.com>
2016-02-19 15:23:58 +0000

    avoid misalignment of value tips in editors

--- client/vtips.js
+++ client/vtips.js
@@ -1,3 +1,4 @@
+'use strict'
 // value tips: hover over a name to see a pop-up with its current value
 var i,t=0,p,w // i:timeout id, t:token counter, p:position as {line,ch}, w:session or editor
 var $b,$t // jQuery wrappers for balloon and triangle
@@ -15,12 +16,13 @@ this.init=function(w0){ // w0:session or editor
 this.processReply=function(x){
   if(x.token!==t||!w)return
   var d=w.getDocument(), dw=w.cm.display.wrapper.clientWidth, r=w.cm.charCoords(p) // r:bounding rect for hovered char
+  var dw=w.cm.display.wrapper.clientWidth, dx=$(w.cm.display.wrapper).offset().left
   var s=(x.tip.length<MH?x.tip:x.tip.slice(0,MH-1).concat('...'))
           .map(function(s){return s.length<MW?s:s.slice(0,MW-3)+'...'}).join('\n')
   cl();$b=$('<div id=vtip-balloon>',d).text(s);$t=$('<div id=vtip-triangle>',d);$b.add($t).hide().appendTo(d.body)
   var th=6,tw=2*th // triangle dimensions
   var bp=8,bw=$b.width(),bh=$b.height() // balloon padding and dimensions
-  var bx=Math.max(0,Math.min(dw-bw,(r.right+r.left-bw)/2-bp)) // bx,by:balloon coordinates
+  var bx=Math.max(dx,Math.min(dx+dw-bw,(r.right+r.left-bw)/2-bp)) // bx,by:balloon coordinates
   var by=r.top-bh-2*bp-th, inv=by<0;if(inv)by=r.bottom+th // inv:is the tooltip upside-down?
   var tx=(r.right+r.left-tw)/2, ty=inv?r.bottom:r.top-th // triangle coordinates
   $b.css({left:bx,top:by});$t.css({left:tx,top:ty});$b.add($t).toggleClass('inv',inv).show()
