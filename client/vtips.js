d7eec3ef2d69ab23c714b49574e117fa6edbc2d1
ngn <nick.nickolov@gmail.com>
2016-02-24 11:47:53 +0000

    some fixes for the positioning of value tips

--- client/vtips.js
+++ client/vtips.js
@@ -22,22 +22,20 @@ this.init=function(w){ // .init(w) gets called for every window w (session or ed
   })
   return rf=function(x){ // return a function that processes the reply
     if(!p)return
-    var d=w.getDocument()
-    var r0=w.cm.charCoords({line:p.line,ch:x.startCol})    // bounding rectangle for start of token
-    var r1=w.cm.charCoords({line:p.line,ch:x.endCol-1})    //                        end   of token
-    var rx=r0.left, ry=r0.top, rw=r1.right-rx, rh=r1.bottom-ry  // whole token
-    rx-=$(w.cm.display.wrapper).offset().left              // correct for CodeMirror's own offset
-    ry-=$(w.cm.display.wrapper).offset().top
-    var dw=w.cm.display.wrapper.clientWidth, dx=$(w.cm.display.wrapper).offset().left // CodeMirror's width and x coord
+    var d=w.getDocument(),ce=w.cm.display.wrapper                // ce:CodeMirror element
+    var cw=ce.clientWidth,co=$(ce).offset(),cx=co.left,cy=co.top // CodeMirror's dimensions and coords
+    var r0=w.cm.charCoords({line:p.line,ch:x.startCol})          // bounding rectangle for start of token
+    var r1=w.cm.charCoords({line:p.line,ch:x.endCol-1})          //                        end   of token
+    var rx=r0.left-cx,ry=r0.top-cy,rw=r1.right-r0.left,rh=r1.bottom-r0.top // whole token
     var s=(x.tip.length<MH?x.tip:x.tip.slice(0,MH-1).concat('...'))
             .map(function(s){return s.length<MW?s:s.slice(0,MW-3)+'...'}).join('\n')
     cl();$b=$('<div id=vtip-balloon>',d).text(s);$t=$('<div id=vtip-triangle>',d);$r=$('<div id=vtip-rect>',d)
     $b.add($t).add($r).hide().appendTo(w.cm.display.wrapper)
-    var th=6,tw=2*th                                       // triangle dimensions
-    var bp=8,bw=$b.width(),bh=$b.height()                  // balloon padding and dimensions
-    var bx=Math.max(dx,Math.min(dx+dw-bw,rx+(rw-bw)/2-bp)) // bx,by:balloon coordinates
-    var by=ry-bh-2*bp-th, inv=by<0;if(inv)by=ry+rh+th      // inv:is the tooltip upside-down?
-    var tx=rx+(rw-tw)/2, ty=inv?ry+rh:ry-th                // triangle coordinates
+    var th=6,tw=2*th                                             // triangle dimensions
+    var bp=8,bw=$b.width(),bh=$b.height()                        // balloon padding and dimensions
+    var bx=Math.max(cx,Math.min(cx+cw-bw,rx+(rw-bw)/2-bp))-cx    // bx,by:balloon coordinates
+    var by=ry-bh-2*bp-th, inv=by<0;if(inv)by=ry+rh+th            // inv:is the tooltip upside-down?
+    var tx=rx+(rw-tw)/2, ty=inv?ry+rh:ry-th                      // triangle coordinates
     $b.css({left:bx,top:by});$t.css({left:tx,top:ty});$b.add($t).toggleClass('inv',inv).show()
     $r.css({left:rx,width:rw,top:ry,height:rh}).show()
   }
