7a5a7910145b3b3beea297787aa983c241d94f2b
ngn <nick.nickolov@gmail.com>
2016-02-29 13:32:03 +0000

    renamed "value tip token" to "... target" and changed default styles

--- client/prefs-colours.js
+++ client/prefs-colours.js
@@ -46,7 +46,7 @@ D.addSyntaxGroups([
   {t:'sel', s:'selection',       c:'.CodeMirror-selected,.CodeMirror-focused .CodeMirror-selected',ctrls:{fg:0,BIU:0}},
   {t:'tc',  s:'tracer',          c:'.tracer .CodeMirror,.tracer .CodeMirror .CodeMirror-gutter-wrapper'},
   {t:'vtip',s:'value tip',       c:'#vtip-balloon,#vtip-triangle',ctrls:{bc:1},skipCSSPrefix:1},
-  {t:'vttk',s:'value tip token', c:'#vtip-rect',ctrls:{bc:1,fg:0,BIU:0},skipCSSPrefix:1}
+  {t:'vtt', s:'value tip target',c:'#vtip-rect',ctrls:{bc:1,fg:0,BIU:0},skipCSSPrefix:1}
 ])
 // Colour schemes have two representations:
 //   in memory                 in localStorage
@@ -86,15 +86,15 @@ var SCMS=[ // built-in schemes
   {name:'Default',styles:'asgn=fg:00f com=fg:088 dfn=fg:00f diam=fg:00f err=fg:f00 fn=fg:008 idm=fg:00f kw=fg:800 '+
     'lnum=fg:008,bg:f,bgo:1 mod=bg:e,bgo:1 mtch=bg:ff8,bgo:.5 norm=bg:f,bgo:1 ns=fg:8 num=fg:8 op1=fg:00f op2=fg:00f '+
     'par=fg:00f quad=fg:808 sel=bg:ddf,bgo:.5 semi=fg:00f sqbr=fg:00f srch=bg:f80,bgo:.5 str=fg:088 tc=bg:d,bgo:1 '+
-    'trad=fg:8 var=fg:8 zld=fg:008 scmd=fg:00f ucmd=fg:00f vttk=bc:f80'},
+    'trad=fg:8 var=fg:8 zld=fg:008 scmd=fg:00f ucmd=fg:00f vtt=bg:ff0'},
   {name:'Francisco Goya',styles:'asgn=fg:ff0 com=fg:b,I:1 cur=bc:f00 dfn2=fg:eb4 dfn3=fg:c79 dfn4=fg:cd0 dfn5=fg:a0d '+
     'dfn=fg:a7b diam=fg:ff0 err=fg:f00,bg:822,bgo:.5,B:1,U:1 fn=fg:0f0 glob=B:1 idm=B:1 kw=fg:aa2 '+
     'lbl=U:1,bg:642,bgo:.5 lnum=fg:b94,bg:010,bgo:1 mod=bg:1,bgo:1 mtch=fg:0,bg:ff8,bgo:.75 norm=fg:9c7,bg:0,bgo:1 '+
     'num=fg:a8b op1=fg:d95 op2=fg:fd6 sel=bg:048,bgo:.5 semi=fg:8 sqbr=fg:8 srch=bg:b96,bgo:.75,fg:0 str=fg:dae '+
-    'tc=bg:1,bgo:1 zld=fg:d9f,B:1 scmd=fg:0ff ucmd=fg:f80,B:1 vtip=bg:733,fg:ff0,bgo:1,bc:900 vttk=bc:f80'},
+    'tc=bg:1,bgo:1 zld=fg:d9f,B:1 scmd=fg:0ff ucmd=fg:f80,B:1 vtip=bg:733,fg:ff0,bgo:1,bc:900 vtt=bc:f80'},
   {name:'Albrecht Dürer',styles:'com=I:1 diam=B:1 err=fg:f,bg:0,bgo:.5,B:1,I:1,U:1 glb=I:1 idm=U:1,bg:e,bgo:.5 kw=B:1 '+
     'lnum=bg:f,bgo:1 mod=bg:e,bgo:1 mtch=bg:c,bgo:.5 norm=bg:f,bgo:1 ns=fg:8 num=fg:8 quad=fg:8 srch=bg:c,bgo:.5 '+
-    'str=fg:8 tc=bg:e,bgo:1 zld=fg:8 vttk=bc:aaa'},
+    'str=fg:8 tc=bg:e,bgo:1 zld=fg:8 vtt=bc:aaa'},
   {name:'Kazimir Malevich',styles:''}
 ].map(decodeScm).map(function(x){x.frozen=1;return x})
 var scms    // all schemes (built-in and user-defined) as objects
