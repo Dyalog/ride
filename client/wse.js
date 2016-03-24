3b50bacc065009aab163134dc5b2460cee1a66bd
ngn <nick.nickolov@gmail.com>
2016-03-24 14:24:24 +0000

    enabled animation in ws explorer

--- client/wse.js
+++ client/wse.js
@@ -3,7 +3,7 @@
 this.WSE=function($e,ide){
   var pending=this.pending={}
   this.$e=$e
-    .jstree({plugins:[],core:{animation:0,check_callback:true,data:function(x,f){
+    .jstree({plugins:[],core:{animation:true,check_callback:true,data:function(x,f){
       var i=x.id==='#'?0:+x.id.replace(/\D+/g,'');pending[i]=f.bind(this);ide.emit('TreeList',{nodeId:i})
     }}})
     .on('click','.jstree-anchor',function(){
@@ -14,7 +14,7 @@ this.WSE=function($e,ide){
 this.WSE.prototype={
   replyTreeList:function(x){
     var f=this.pending[x.nodeId];if(!f)return
-    f(x.nodeIds.map(function(c,i){return{text:x.names[i],children:!!c,id:'wse-'+(c||('leaf-'+x.nodeId+'-'+i))}}))
+    f((x.nodeIds||[]).map(function(c,i){return{text:x.names[i],children:!!c,id:'wse-'+(c||('leaf-'+x.nodeId+'-'+i))}}))
     delete this.pending[x.nodeId]
   },
   refresh:function(){this.$e.jstree('refresh')}
