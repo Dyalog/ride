8453c0ca2d04b7481a35607d4601f31ab09eee37
ngn <nick.nickolov@gmail.com>
2016-03-24 14:13:32 +0000

    refresh node in ws explorer on click

--- client/wse.js
+++ client/wse.js
@@ -6,12 +6,15 @@ this.WSE=function($e,ide){
     .jstree({plugins:[],core:{animation:0,check_callback:true,data:function(x,f){
       var i=x.id==='#'?0:+x.id.replace(/\D+/g,'');pending[i]=f.bind(this);ide.emit('TreeList',{nodeId:i})
     }}})
-    .on('click','.jstree-anchor',function(){ide.emit('Edit',{win:0,pos:0,text:$e.jstree('get_path',this,'.')})})
+    .on('click','.jstree-anchor',function(){
+      ide.emit('Edit',{win:0,pos:0,text:$e.jstree('get_path',this,'.')})
+      ;/^wse-leaf-/.test(this.id)||$e.jstree('refresh_node',this)
+    })
 }
 this.WSE.prototype={
   replyTreeList:function(x){
     var f=this.pending[x.nodeId];if(!f)return
-    f(x.nodeIds.map(function(c,i){return{text:x.names[i],children:!!c,id:'wse-'+(c||(x.nodeId+'-'+i))}}))
+    f(x.nodeIds.map(function(c,i){return{text:x.names[i],children:!!c,id:'wse-'+(c||('leaf-'+x.nodeId+'-'+i))}}))
     delete this.pending[x.nodeId]
   },
   refresh:function(){this.$e.jstree('refresh')}
