b8601674c1ddb9e7d6600c31b1b81b81db6285dd
Nick <nick@dyalog.com>
2016-05-10 16:49:25 +0100

    simplified svg and rasterization script

--- style/img/fold/_.sh
+++ style/img/fold/_.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-for x in plus minus cont last; do
-  p=${x}.png; rm -f $p; inkscape _.svg -C -j -e$p -i$x -w16; exiftool -all= -overwrite_original_in_place $p
-done
+rm -f *.png
+for x in plus minus cont last; do inkscape _.svg -C -j -e${x}.png -i$x -w16; done
+exiftool -all= -overwrite_original_in_place *.png
