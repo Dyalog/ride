8dbdad60354f03727a006dc750a5cb694aaf2213
Nick <nick@dyalog.com>
2016-05-11 11:48:15 +0100

    use new svg icons for fold gutter

--- style/img/fold/_.sh
+++ style/img/fold/_.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 rm -f *.png
-for x in plus minus cont last; do inkscape _.svg -C -j -e${x}.png -i$x -w16; done
+for x in open folded cont end; do inkscape _.svg -C -j -e${x}.png -i$x -w16; done
 exiftool -all= -overwrite_original_in_place *.png
