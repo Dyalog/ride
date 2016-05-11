7b8e3fe459c114b298ad5a32711ee87f02a19fcf
Nick <nick@dyalog.com>
2016-05-11 15:33:13 +0100

    scrape dfns.dyalog.com/n_kbmac.htm for Mac kbds

--- tools/kbds-gen.js
+++ tools/kbds-gen.js
@@ -58,34 +58,39 @@ var G={ // geometries http://www.abreojosensamblador.net/Productos/AOE/html/Pags
     ]
   }
 }
-get('dfns.dyalog.com','/n_keyboards.htm',function(data){
-  var geom={_:'iso'},layouts={}
-  cheerio.load(data)('pre').text()
-    .replace(/\r\n/g,'\n')
-    .replace(/\nDyalog APL\/([A-Z]{2}).*\n¯*\n(┌(?:.*\n){12}.*)/gm,function(_,lc,desc){
-      var l=layouts[lc]=[];for(var i=0;i<4;i++){l.push([]);for(var j=0;j<58;j++)l[i].push(' ')}
-      var g;for(var g1 in G)if(desc.match(G[g1].re)){g=g1;g!=='iso'&&(geom[lc]=g);break}
-      g||err('unrecognized geometry for '+lc+' layout')
-      var lines=desc.split('\n')
-      // r,c: coords of the key in the keyboard    x,y: coords of the symbol on the key
-      for(var r=0;r<4;r++)for(var y=0;y<2;y++){
-        var chunks=lines[1+y+3*r].slice(1,74).split(/[─│┌┬┐├┼┤└┴┘]+/g)
-        for(var c=0;c<chunks.length;c++)if(G[g1].sc[r][c]){
-          var chunk=chunks[c]
-          if(chunk[1]!==' '||chunk[3]!==' ')err('bad key in '+lc+' layout -- '+JSON.stringify(chunk))
-          for(var x=0;x<2;x++)l[2*x+1-y][G[g1].sc[r][c]]=chunk[2*x]
-        }
-      }
-      for(var i=0;i<4;i++)l[i]=l[i].join('')
-      console.assert(l[0].length==l[1].length&&l[0].length==l[2].length&&l[0].length==l[3].length)
+get('dfns.dyalog.com','/n_keyboards.htm',function(data0){
+  get('dfns.dyalog.com','/n_kbmac.htm',function(data1){
+    var geom={_:'iso'},layouts={}
+    ;[data0,data1].forEach(function(data){
+      cheerio.load(data)('pre').text()
+        .replace(/\r\n/g,'\n')
+        .replace(/\nDyalog( Mac)? APL\/([A-Z]{2}).*\n[¯\-]*\n(┌(?:.*\n){12}.*)/gm,function(_,mac,lc,desc){
+          if(mac)lc+='-Mac'
+          var l=layouts[lc]=[];for(var i=0;i<4;i++){l.push([]);for(var j=0;j<58;j++)l[i].push(' ')}
+          var g;for(var g1 in G)if(desc.match(G[g1].re)){g=g1;g!=='iso'&&(geom[lc]=g);break}
+          g||err('unrecognized geometry for '+lc+' layout')
+          var lines=desc.split('\n')
+          // r,c: coords of the key in the keyboard    x,y: coords of the symbol on the key
+          for(var r=0;r<4;r++)for(var y=0;y<2;y++){
+            var chunks=lines[1+y+3*r].slice(1,74).split(/[─│┌┬┐├┼┤└┴┘]+/g)
+            for(var c=0;c<chunks.length;c++)if(G[g1].sc[r][c]){
+              var chunk=chunks[c]
+              if(chunk[1]!==' '||chunk[3]!==' ')err('bad key in '+lc+' layout -- '+JSON.stringify(chunk))
+              for(var x=0;x<2;x++)l[2*x+1-y][G[g1].sc[r][c]]=chunk[2*x]
+            }
+          }
+          for(var i=0;i<4;i++)l[i]=l[i].join('')
+          console.assert(l[0].length==l[1].length&&l[0].length==l[2].length&&l[0].length==l[3].length)
+        })
     })
-  fs.writeFileSync('../client/kbds.js',
-    '// generated code, do not edit\n'+
-    'this.geom='+JSON.stringify(geom)+'\n'+
-    'this.layouts={\n'+
-    '  '+Object.keys(layouts).sort().map(function(lc){
-           var l=layouts[lc];return lc+':[\n   '+l.map(JSON.stringify).join(',\n   ')+'\n  ]'
-         }).join(',\n  ')+'\n'+
-    '};\n'
-  )
+    fs.writeFileSync('../client/kbds.js',
+      '// generated code, do not edit\n'+
+      'this.geom='+JSON.stringify(geom)+'\n'+
+      'this.layouts={\n'+
+      '  '+Object.keys(layouts).sort().map(function(lc){
+             var l=layouts[lc];return JSON.stringify(lc)+':[\n   '+l.map(JSON.stringify).join(',\n   ')+'\n  ]'
+           }).join(',\n  ')+'\n'+
+      '};\n'
+    )
+  })
 })
