83a55c09f8d1faa8902c290d7c73c89959da0f95
Nick <nick@dyalog.com>
2016-06-01 10:13:07 +0100

    rewrote server.js and proxy.js in ES6

--- proxy.js
+++ proxy.js
@@ -1,50 +1,51 @@
 'use strict'
-var fs=require('fs'),net=require('net'),os=require('os'),path=require('path'),cp=require('child_process')
-var log;(function(){
+let fs=require('fs'),net=require('net'),os=require('os'),path=require('path'),cp=require('child_process')
+let log
+{
   // record no more than N log messages per T milliseconds; at any moment, there have been n messages since time t
-  var stdoutOK=1,N=500,T=1000,n=0,t=0,t0=+new Date,l=[] // l:listeners
-  log=function(s){
-    var t1=+new Date;if(t1-t>T){t=t1;n=1} // if last message was too long ago, start counting afresh
-    var m= ++n<N ? (t1-t0)+' '+s+'\n' : n===N ? '... logging temporarily suppressed\n' : 0; if(!m)return
+  let stdoutOK=1,N=500,T=1000,n=0,t=0,t0=+new Date,l=[] // l:listeners
+  log=(s)=>{
+    let t1=+new Date;if(t1-t>T){t=t1;n=1} // if last message was too long ago, start counting afresh
+    let m= ++n<N ? (t1-t0)+' '+s+'\n' : n===N ? '... logging temporarily suppressed\n' : 0; if(!m)return
     if(stdoutOK)try{process.stdout&&process.stdout.write&&process.stdout.write(m)}catch(_){stdoutOK=0}
-    var l1=l.slice(0);for(var i=0;i<l1.length;i++)l1[i](m) // notify listeners, prevent concurrent modification
+    let l1=l.slice(0);for(let i=0;i<l1.length;i++)l1[i](m) // notify listeners, prevent concurrent modification
   }
-  log.addListener=function(x){l.push(x)}
-  log.rmListener=function(x){var i=l.indexOf(x);i>=0&&l.splice(i,1)}
-  var f=process.env.RIDE_LOG
+  log.addListener=(x)=>{l.push(x)}
+  log.rmListener=(x)=>{let i=l.indexOf(x);i>=0&&l.splice(i,1)}
+  let f=process.env.RIDE_LOG
   if(f){ // if $RIDE_LOG is present, also log to a file (in addition to stdout)
-    var h=process.env.HOME||process.env.USERPROFILE;if(h)f=path.resolve(h,f)
-    fd=fs.openSync(f,'a');l.push(function(s){var b=Buffer(m);fs.writeSync(fd,b,0,b.length)})
+    let h=process.env.HOME||process.env.USERPROFILE;if(h)f=path.resolve(h,f)
+    fd=fs.openSync(f,'a');l.push((s)=>{let b=Buffer(m);fs.writeSync(fd,b,0,b.length)})
   }
   if(typeof window!=='undefined'){ // are we running under NW.js as opposed to just NodeJS?
-    var i=0,a=Array(1000)          // if so, store latest log messages in RAM
-    log.get=function(){return a.slice(i).concat(a.slice(0,i))}
-    l.push(function(s){a[i++]=s;i%=a.length})
+    let i=0,a=Array(1000)          // if so, store latest log messages in RAM
+    log.get=()=>a.slice(i).concat(a.slice(0,i))
+    l.push((s)=>{a[i++]=s;i%=a.length})
   }
   log(new Date().toISOString())
-}())
-var clt,   // client, TCP connection to interpreter
+}
+let clt,   // client, TCP connection to interpreter
     skt,   // websocket or other connection-like object for communicating with the browser
     child, // a ChildProcess instance, the result from spawn()
     srv    // server, used to listen for connections from interpreters
-function trunc(s){return s.length>1000?s.slice(0,997)+'...':s}
-function ls(x){return fs.readdirSync(x)}
-function sil(f){return function(x){try{f(x)}catch(_){}}} // exception silencer
-function shEsc(x){return"'"+x.replace(/'/g,"'\\''")+"'"} // shell escape
-function parseVer(s){return s.split('.').map(function(x){return+x})}
-function send(s){if(clt){log('send '+trunc(s));var b=Buffer('xxxxRIDE'+s);b.writeInt32BE(b.length,0);clt.write(b)}}
-function cmd(c,h){send(JSON.stringify([c,h||{}]))} // c:command name, h:arguments as a JS object
-function toBrowser(x,y){log('to browser:'+trunc(JSON.stringify([x,y])));skt&&skt.emit(x,y)}
-function initInterpreterConn(){
-  var P=process.env.RIDE_PROTOCOL||'2'
-  var q=Buffer(0), old // old: have we warned the user that we're talking to an old interpreter
-  clt.on('data',function(x){
-    q=Buffer.concat([q,x]);var n
+let trunc=(s)=>s.length>1000?s.slice(0,997)+'...':s
+let ls=(x)=>fs.readdirSync(x)
+let sil=(f)=>(x)=>{try{f(x)}catch(_){}} // exception silencer
+let shEsc=(x)=>"'"+x.replace(/'/g,"'\\''")+"'" // shell escape
+let parseVer=(s)=>s.split('.').map((x)=>+x)
+let send=(s)=>{if(clt){log('send '+trunc(s));let b=Buffer('xxxxRIDE'+s);b.writeInt32BE(b.length,0);clt.write(b)}}
+let cmd=(c,h)=>{send(JSON.stringify([c,h||{}]))} // c:command name, h:arguments as a JS object
+let toBrowser=(x,y)=>{log('to browser:'+trunc(JSON.stringify([x,y])));skt&&skt.emit(x,y)}
+let initInterpreterConn=()=>{
+  let P=process.env.RIDE_PROTOCOL||'2'
+  let q=Buffer(0), old // old: have we warned the user that we're talking to an old interpreter
+  clt.on('data',(x)=>{
+    q=Buffer.concat([q,x]);let n
     while(q.length>=4&&(n=q.readInt32BE(0))<=q.length){
       if(n<=8){toBrowser('*error',{msg:''});break}
-      var m=''+q.slice(8,n);q=q.slice(n);log('recv '+trunc(m))
+      let m=''+q.slice(8,n);q=q.slice(n);log('recv '+trunc(m))
       if(m[0]==='['){
-        var u=JSON.parse(m);skt&&skt.emit(u[0],u[1])
+        let u=JSON.parse(m);skt&&skt.emit(u[0],u[1])
       }else if(/^<ReplyUnknownRIDECommand>/.test(m)&&!old){
         old=1;toBrowser('*error',{msg:'This version of RIDE cannot talk to interpreters older than v15.0'})
       }else if(/^UsingProtocol=/.test(m)&&m.slice(m.indexOf('=')+1)!==P){
@@ -52,21 +53,21 @@ function initInterpreterConn(){
       }
     }
   })
-  clt.on('error',function(e){toBrowser('*error',{msg:''+e});clt=null})
-  clt.on('end',function(){log('interpreter diconnected');toBrowser('*disconnected');clt=null})
+  clt.on('error',(e)=>{toBrowser('*error',{msg:''+e});clt=null})
+  clt.on('end',()=>{log('interpreter diconnected');toBrowser('*disconnected');clt=null})
   send('SupportedProtocols='+P);send('UsingProtocol='+P)
   cmd('Identify',{identity:1});cmd('Connect',{remoteId:2});cmd('GetWindowLayout')
 }
-var handlers={
-  '*connect':function(x){
-    var m=net,o={host:x.host,port:x.port} // m:module used to create connection; o:options for .connect()
+let handlers={
+  '*connect':(x)=>{
+    let m=net,o={host:x.host,port:x.port} // m:module used to create connection; o:options for .connect()
     if(x.ssl){
       m=require('tls');o.rejectUnauthorized=false
       if(x.cert)try{o.key=fs.readFileSync(x.cert)}catch(e){toBrowser('*error',{msg:e.message});return}
     }
-    clt=m.connect(o,function(){
+    clt=m.connect(o,()=>{
       if(x.ssl&&x.subj){
-        var s=clt.getPeerCertificate().subject.CN
+        let s=clt.getPeerCertificate().subject.CN
         if(s!==x.subj){
           toBrowser('*error',{msg:
             'Wrong server certificate name.  Expected:'+JSON.stringify(x.subj)+', actual:'+JSON.stringify(s)})
@@ -75,72 +76,72 @@ var handlers={
       }
       toBrowser('*connected',x);initInterpreterConn()
     })
-    clt.on('error',function(err){log('connect failed: '+err);clt=null;toBrowser('*error',{msg:err.message})})
+    clt.on('error',(err)=>{log('connect failed: '+err);clt=null;toBrowser('*error',{msg:err.message})})
   },
-  '*launch':function(x){
-    var exe=(x||{}).exe||process.env.RIDE_INTERPRETER_EXE||'dyalog'
-    srv=net.createServer(function(c){
-      log('spawned interpreter connected');var a=srv.address();srv&&srv.close();srv=null;clt=c
+  '*launch':(x)=>{
+    let exe=(x||{}).exe||process.env.RIDE_INTERPRETER_EXE||'dyalog'
+    srv=net.createServer((c)=>{
+      log('spawned interpreter connected');let a=srv.address();srv&&srv.close();srv=null;clt=c
       toBrowser('*connected',{host:a.address,port:a.port});initInterpreterConn()
       if(typeof window!=='undefined')window.D.lastSpawnedExe=exe
     })
-    srv.on('error',function(err){log('listen failed: '+err);srv=clt=null;toBrowser('*error',{msg:err.message})})
-    srv.listen(0,'127.0.0.1',function(){
-      var a=srv.address(),hp=a.address+':'+a.port
+    srv.on('error',(err)=>{log('listen failed: '+err);srv=clt=null;toBrowser('*error',{msg:err.message})})
+    srv.listen(0,'127.0.0.1',()=>{
+      let a=srv.address(),hp=a.address+':'+a.port
       log('listening for connections from spawned interpreter on '+hp)
       log('spawning interpreter '+JSON.stringify(exe))
-      var args=['+s','-q'],stdio=['pipe','ignore','ignore']
+      let args=['+s','-q'],stdio=['pipe','ignore','ignore']
       if(/^win/i.test(process.platform)){args=[];stdio[0]='ignore'}
       try{
-        var h=x.env||{},H=process.env;for(var k in H)h[k]=H[k];h.RIDE_INIT='CONNECT:'+hp;h.RIDE_SPAWNED='1'
+        let h=x.env||{},H=process.env;for(let k in H)h[k]=H[k];h.RIDE_INIT='CONNECT:'+hp;h.RIDE_SPAWNED='1'
         child=cp.spawn(exe,args,{stdio:stdio,env:h})
       }catch(e){
         toBrowser('*error',{code:0,msg:''+e});return
       }
       toBrowser('*spawned',{exe:exe,pid:child.pid})
-      child.on('error',function(err){
+      child.on('error',(err)=>{
         srv&&srv.close();srv=clt=child=null
         toBrowser('*error',{code:err.code,msg:err.code==='ENOENT'?"Cannot find the interpreter's executable":''+err})
       })
-      child.on('exit',function(code,sig){
+      child.on('exit',(code,sig)=>{
         srv&&srv.close();srv=clt=null;toBrowser('*spawnedExited',{code:code,sig:sig});child=null
       })
     })
   },
-  '*sshFetchListOfInterpreters':function(x){
-    var c=sshExec(x,'/bin/ls /opt/mdyalog/*/*/*/mapl /Applications/Dyalog-*/Contents/Resources/Dyalog/mapl',function(sm){
-      var s=''
-      sm.on('data',function(x){s+=x}).on('close',function(){
-        toBrowser('*sshInterpreters',{interpreters:s.split('\n').filter(function(x){return x}).map(function(x){
-          var a=x.split('/')
+  '*sshFetchListOfInterpreters':(x)=>{
+    let c=sshExec(x,'/bin/ls /opt/mdyalog/*/*/*/mapl /Applications/Dyalog-*/Contents/Resources/Dyalog/mapl',(sm)=>{
+      let s=''
+      sm.on('data',(x)=>{s+=x}).on('close',()=>{
+        toBrowser('*sshInterpreters',{interpreters:s.split('\n').filter((x)=>x).map((x)=>{
+          let a=x.split('/')
           return a[1]==='opt'?{exe:x,ver:parseVer(a[3]),bits:+a[4],edition:a[5]}
                              :{exe:x,ver:parseVer(a[2].replace(/^Dyalog-|\.app$/g,'')),bits:64,edition:'unicode'}
         })})
         c.end()
       })
-    }).on('error',function(x){toBrowser('*error',{msg:x.message||''+x})
-                              toBrowser('*sshInterpreters',{interpreters:[]})})
+    }).on('error',(x)=>{toBrowser('*error',{msg:x.message||''+x})
+                        toBrowser('*sshInterpreters',{interpreters:[]})})
   },
-  '*ssh':function(x){
-    var c=sshExec(x,'/bin/sh',function(sm){
-      sm.on('close',function(code,sig){toBrowser('*sshExited',{code:code,sig:sig});c.end()})
-      c.forwardIn('',0,function(err,remotePort){if(err)throw err
-        var s='';for(var k in x.env)s+=k+'='+shEsc(x.env[k])+' '
+  '*ssh':(x)=>{
+    let c=sshExec(x,'/bin/sh',(sm)=>{
+      sm.on('close',(code,sig)=>{toBrowser('*sshExited',{code:code,sig:sig});c.end()})
+      c.forwardIn('',0,(err,remotePort)=>{if(err)throw err
+        let s='';for(let k in x.env)s+=k+'='+shEsc(x.env[k])+' '
         sm.write(s+'RIDE_INIT=CONNECT:127.0.0.1:'+remotePort+' '+shEsc(x.exe||'dyalog')+' +s -q >/dev/null\n')
       })
-    }).on('error',function(x){toBrowser('*error',{msg:x.message||''+x})})
+    }).on('error',(x)=>{toBrowser('*error',{msg:x.message||''+x})})
   },
-  '*listen':function(x){
-    srv=net.createServer(function(c){
-      var t,rhost=c&&(t=c.request)&&(t=t.connection)&&t.remoteAddress
+  '*listen':(x)=>{
+    srv=net.createServer((c)=>{
+      let t,rhost=c&&(t=c.request)&&(t=t.connection)&&t.remoteAddress
       log('interpreter connected from '+rhost);srv&&srv.close();srv=null;clt=c
       toBrowser('*connected',{host:rhost,port:x.port});initInterpreterConn()
     })
-    srv.on('error',function(err){srv=null;toBrowser('*error',{msg:''+err})})
-    srv.listen(x.port,x.host||'',function(){log('listening on port '+x.port);x.callback&&x.callback()})
+    srv.on('error',(err)=>{srv=null;toBrowser('*error',{msg:''+err})})
+    srv.listen(x.port,x.host||'',()=>{log('listening on port '+x.port);x.callback&&x.callback()})
   },
-  '*listenCancel':function(){srv&&srv.close()},
-  '*getProxyInfo':function(){
+  '*listenCancel':()=>{srv&&srv.close()},
+  '*getProxyInfo':()=>{
     // List available interpreter executables, possible paths are:
     //   C:\Program Files\Dyalog\Dyalog APL $VERSION\dyalog.exe
     //   C:\Program Files\Dyalog\Dyalog APL $VERSION unicode\dyalog.exe
@@ -150,12 +151,12 @@ var handlers={
     //   C:\Program Files (x86)\Dyalog\Dyalog APL $VERSION unicode\dyalog.exe
     //   /opt/mdyalog/$VERSION/[64|32]/[classic|unicode]/mapl
     //   /Applications/Dyalog-$VERSION/Contents/Resources/Dyalog/mapl
-    var r={interpreters:[],platform:process.platform} // proxy info (the result)
+    let r={interpreters:[],platform:process.platform} // proxy info (the result)
     if(/^win/.test(r.platform)){
       try{
-        cp.exec('reg query "HKEY_CURRENT_USER\\Software\\Dyalog" /s /v localdyalogdir',{timeout:2000},function(err,s){
-          var b,v,u,m // b:bits,v:version,u:edition,m:match object
-          err||s.split('\r\n').forEach(function(l){if(l){ // l:current line
+        cp.exec('reg query "HKEY_CURRENT_USER\\Software\\Dyalog" /s /v localdyalogdir',{timeout:2000},(err,s)=>{
+          let b,v,u,m // b:bits,v:version,u:edition,m:match object
+          err||s.split('\r\n').forEach((l)=>{if(l){ // l:current line
             if(m=/^HK.*\\Dyalog APL\/W(-64)? (\d+\.\d+)( Unicode)?$/i.exec(l)){
               b=m[1]?64:32;v=m[2];u=m[3]?'unicode':'classic'
             }else if(v&&(m=/^ *localdyalogdir +REG_SZ +(\S.*)$/i.exec(l))){
@@ -171,20 +172,20 @@ var handlers={
       }
     }else if(r.platform==='darwin'){
       try{
-        var a='/Applications'
-        ls(a).forEach(function(x){
-          var m=/^Dyalog-(\d+\.\d+)\.app$/.exec(x), exe=a+'/'+x+'/Contents/Resources/Dyalog/mapl'
+        let a='/Applications'
+        ls(a).forEach((x)=>{
+          let m=/^Dyalog-(\d+\.\d+)\.app$/.exec(x), exe=a+'/'+x+'/Contents/Resources/Dyalog/mapl'
           m&&fs.existsSync(exe)&&r.interpreters.push({exe:exe,ver:parseVer(m[1]),bits:64,edition:'unicode'})
         })
       }catch(_){}
       toBrowser('*proxyInfo',r)
     }else{
       try{
-        var a='/opt/mdyalog'
-        ls(a).forEach(sil(function(v){if(/^\d+\.\d+/.test(v))
-          ls(a+'/'+v).forEach(sil(function(b){if(b==='32'||b==='64')
-            ls(a+'/'+v+'/'+b).forEach(sil(function(u){if(u==='unicode'||u==='classic'){
-              var exe=a+'/'+v+'/'+b+'/'+u+'/mapl'
+        let a='/opt/mdyalog'
+        ls(a).forEach(sil((v)=>{if(/^\d+\.\d+/.test(v))
+          ls(a+'/'+v).forEach(sil((b)=>{if(b==='32'||b==='64')
+            ls(a+'/'+v+'/'+b).forEach(sil((u)=>{if(u==='unicode'||u==='classic'){
+              let exe=a+'/'+v+'/'+b+'/'+u+'/mapl'
               fs.existsSync(exe)&&r.interpreters.push({exe:exe,ver:parseVer(v),bits:+b,edition:u})
             }}))
           }))
@@ -194,21 +195,21 @@ var handlers={
     }
   }
 }
-function sshExec(x,cmd,callback){
+let sshExec=(x,cmd,callback)=>{
   try{ // see https://github.com/mscdex/ssh2/issues/238#issuecomment-87495628 for why we use tryKeyboard:true
-    var c=new(require('ssh2').Client),o={host:x.host,port:x.port,username:x.user,tryKeyboard:true}
+    let c=new(require('ssh2').Client),o={host:x.host,port:x.port,username:x.user,tryKeyboard:true}
     x.key?(o.privateKey=fs.readFileSync(x.key)):(o.password=x.pass)
-    c.on('ready',function(){c.exec(cmd,function(err,sm){err||callback(sm)})})
-     .on('tcp connection',function(_,acc){clt=acc();toBrowser('*connected',{host:'',port:0});initInterpreterConn()})
-     .on('keyboard-interactive',function(_,_1,_2,_3,finish){finish([x.pass])})
+    c.on('ready',()=>{c.exec(cmd,(err,sm)=>{err||callback(sm)})})
+     .on('tcp connection',(_,acc)=>{clt=acc();toBrowser('*connected',{host:'',port:0});initInterpreterConn()})
+     .on('keyboard-interactive',(_,_1,_2,_3,finish)=>{finish([x.pass])})
      .connect(o)
   }catch(e){toBrowser('*error',{msg:e.message})}
   return c
 }
-module.exports=function(x){
+module.exports=(x)=>{
   log('browser connected')
-  ;(skt=x).onevent=function(x){
-    var f=handlers[x.data[0]];f&&log('from browser:'+trunc(JSON.stringify(x.data)))
+  ;(skt=x).onevent=(x)=>{
+    let f=handlers[x.data[0]];f&&log('from browser:'+trunc(JSON.stringify(x.data)))
     f?f(x.data[1]):cmd(x.data[0],x.data[1]||{})
   }
   child&&toBrowser('*spawned',{pid:child.pid})
--- server.js
+++ server.js
@@ -1,44 +1,37 @@
 #!/usr/bin/env node
-
-// Environment variables:
-//   RIDE_SERVER_WATCH=1     watch for changes and rebuild automatically (useful for development)
-//   RIDE_SERVER_INSECURE=1  use http (on port 8000) instead of https (on port 8443)
-//   RIDE_SERVER_IPV6=1      use IPv6
-
+// $RIDE_SERVER_WATCH=1     watch for changes and rebuild automatically (useful for development)
+// $RIDE_SERVER_INSECURE=1  use http (on port 8000) instead of https (on port 8443)
+// $RIDE_SERVER_IPV6=1      use IPv6
 'use strict'
-var compression=require('compression'),express=require('express'),fs=require('fs'),http=require('http'),
-    https=require('https'),WSServer=require('ws').Server,proxy=require('./proxy'),spawn=require('child_process').spawn
-
-var t0=+new Date
-function log(s){process.stdout.write((new Date-t0)+': '+s+'\n')}
-
-var _d=__dirname,tid,busy
-function build(){
+let compression=require('compression'),express=require('express'),fs=require('fs'),http=require('http'),
+    https=require('https'),WSServer=require('ws').Server,proxy=require('./proxy'),{spawn}=require('child_process')
+let t0=+new Date,log=(s)=>{process.stdout.write((new Date-t0)+': '+s+'\n')}
+let _d=__dirname,tid,busy
+let build=()=>{
   if(busy){tid=setTimeout(build,100);return}
-  log('building');tid=0;busy=1;spawn(_d+'/build.sh',{cwd:_d,stdio:'inherit'}).on('close',function(){log('done');busy=0})
+  log('building');tid=0;busy=1;spawn(_d+'/build.sh',{cwd:_d,stdio:'inherit'}).on('close',()=>{log('done');busy=0})
 }
 if(process.env.RIDE_SERVER_WATCH){
-  ;['.','client','style','style/img','style/themes'].forEach(function(x){
-    fs.watch(_d+'/'+x,function(){tid=tid||setTimeout(build,100)})
-  })
+  ;['.','client','style','style/img','style/themes']
+    .forEach((x)=>{fs.watch(_d+'/'+x,()=>{tid=tid||setTimeout(build,100)})})
   build()
 }
 
-var app=express()
+let app=express()
 app.disable('x-powered-by')
-app.use(function(req,res,next){log(req.method+' '+req.path);next()})
+app.use((req,res,next)=>{log(req.method+' '+req.path);next()})
 app.use(compression())
-app.use('/',express.static('build/static'))
-var insecure=process.env.RIDE_SERVER_INSECURE
-var port=insecure?8000:8443
-var server=insecure?http.createServer(app)
+app.use('/',express.static('.'))
+let insecure=process.env.RIDE_SERVER_INSECURE
+let port=insecure?8000:8443
+let server=insecure?http.createServer(app)
              :https.createServer({cert:fs.readFileSync('ssl/cert.pem'),key:fs.readFileSync('ssl/key.pem')},app)
-;(new WSServer({server:server})).on('connection',function(ws){
-  var l={},io={} // l:listeners, io:socket.io-like interface
-  io.emit=function(x,y){ws.send(JSON.stringify([x,y]));return io}
-  io.on=function(e,f){(l[e]=l[e]||[]).push(f);return io}
-  io.onevent=function(x){var a=l[x.data[0]]||[];for(var i=0;i<a.length;i++)a[i].apply(null,x.data.slice(1))}
-  ws.on('message',function(m){io.onevent({data:JSON.parse(m)})})
+;(new WSServer({server:server})).on('connection',(ws)=>{
+  let l={},io={} // l:listeners, io:socket.io-like interface
+  io.emit=(x,y)=>{ws.send(JSON.stringify([x,y]));return io}
+  io.on=(e,f)=>{(l[e]=l[e]||[]).push(f);return io}
+  io.onevent=(x)=>{let a=l[x.data[0]]||[];for(let i=0;i<a.length;i++)a[i].apply(null,x.data.slice(1))}
+  ws.on('message',(m)=>{io.onevent({data:JSON.parse(m)})})
   proxy(io)
 })
-server.listen(port,process.env.RIDE_SERVER_IPV6?'::':'0.0.0.0',function(){log('server started on port '+port)})
+server.listen(port,process.env.RIDE_SERVER_IPV6?'::':'0.0.0.0',()=>{log('server started on port '+port)})
