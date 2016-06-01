d68055e7000db54b6e84ac563f936a860b81db82
Nick <nick@dyalog.com>
2016-06-01 13:58:56 +0100

    simplified build

--- build.sh
+++ build.sh
@@ -3,10 +3,9 @@ set -e -o pipefail
 export PATH="$(dirname "$0")/node_modules/.bin:$PATH"
 cd "$(dirname "$0")"
 if [ ! -e node_modules ]; then npm i; fi
-mkdir -p build/{js/client,nw,nw/themes,nw/jstree,tmp,static}
+mkdir -p build/{nw,nw/themes,nw/jstree,tmp,static}
 
 cp -uvr *.html proxy.js style/{*.png,apl385.woff,img,jstree} favicon.ico package.json build/nw/
-cp -uvr client build/js/
 
 i=style/style.less o=build/nw/style.css
 if [ ! -e $o -o $(find "$(dirname $i)" -type f -newer $o 2>/dev/null | wc -l) -gt 0 ]; then
