9ca8019b988957f4147049ae2bd16963fca9175e
Nick <nick@dyalog.com>
2016-06-23 16:38:34 +0100

    About dialog using the same dialog implementation as Prefs

--- src/abt.js
+++ src/abt.js
@@ -1,8 +1,20 @@
 //About dialog
+;(function(){
+
+var d,ta //DOM elements for the dialog and the textarea
 D.abt=function(){
+  if(!d){
+    d=document.getElementById('abt');D.util.initDlg(d)
+    document.getElementById('abt_close').onclick=function(){d.hidden=1}
+    document.getElementById('abt_copy').onclick=function(){D.el.clipboard.writeText(ta.value)}
+    document.getElementById('abt_contact').onclick=
+      function(x){if(x.target.nodeName==='A'&&/^http/.test(x.target.href)){D.openExternal(x.target.href);return!1}}
+    ta=document.getElementById('abt_ta')
+  }
+  d.hidden=0
   var v=D.versionInfo,ri=D.remoteIdentification||{},u='unknown',db=D.db||localStorage,repr=JSON.stringify
   var s='';for(var i=0;i<db.length;i++){var x=db.key(i);s+=(i?',\n':'')+'    '+repr(x)+':'+repr(db.getItem(x))}
-  var info='IDE:'+
+  ta.value='IDE:'+
     '\n  Version: '   +(v.version          ||u)+
     '\n  Platform: '  +(navigator.platform ||u)+
     '\n  Date: '      +(v.date             ||u)+
@@ -14,11 +26,7 @@ D.abt=function(){
     '\n  Platform: '  +(ri.platform        ||u)+
     '\n  Edition: '   +(ri.arch            ||u)+
     '\n  Date: '      +(ri.date            ||u)+'\n'
-  var abt=document.getElementById('abt')
-  var ta =document.getElementById('abt_ta')
-  var btns=[{html:'C<u>l</u>ose',click:function(){$(abt).dialog('close')}}]
-  D.el&&btns.unshift({html:'<u>C</u>opy',click:function(){D.el.clipboard.writeText(ta.value)}})
-  $(abt).dialog({title:'About',width:520,height:410,buttons:btns,open:function(){ta.focus()}})
-  abt.onclick=function(x){if(x.target.nodeName==='A'&&/^http/.test(x.target.href)){D.openExternal(x.target.href);return!1}}
-  ta.value=info;ta.select()
+  ta.focus();ta.select()
 }
+
+}())
