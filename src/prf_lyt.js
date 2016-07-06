8ea991838109debac5abca9529af7d5788ef0f71
Nick <nick@dyalog.com>
2016-07-06 11:44:23 +0100

    prototype for Preferences>Layout>Legend

--- src/prf_lyt.js
+++ src/prf_lyt.js
@@ -9,7 +9,7 @@ D.prf_tabs.lyt={
   name:'Layout',
   init:function(t){
     var sk={15:'←',16:'↹',30:'Caps',43:'↲',44:'⇧',57:'⇧'} //special keys
-    var a=t.querySelectorAll('[id^="lyt_"]');for(var i=0;i<a.length;i++)q[a[i].id.replace(/^lyt_/,'')]=a[i]
+    var a=document.querySelectorAll('[id^="lyt_"]');for(var i=0;i<a.length;i++)q[a[i].id.replace(/^lyt_/,'')]=a[i]
     var s='';for(var i=1;i<NK;i++)s+='<span id=k'+i+' class=key>'+(sk[i]||'<span class=g2></span><input class=g3><br>'+
                                                                           '<span class=g0></span><input class=g1>')+'</span>'
     q.kbd.innerHTML=s;q.lc.innerHTML='<option>'+Object.keys(layouts).sort().join('<option>')
@@ -31,14 +31,15 @@ D.prf_tabs.lyt={
       var d=Object.keys(layouts).filter(function(x){return x.slice(3,5)===c}).sort()[0] //default layout for country c
       D.prf.kbdLocale(D.mac&&layouts[l+'_'+c+'_Mac']?l+'_'+c+'_Mac':layouts[l+'_'+c]?l+'_'+c:d?d:'en_US')
     }
-    q.rst.onclick=function(){var lc=q.lc.value;q.pfx.value=D.prf.prefixKey.getDefault()//;updLegend()
+    q.rst.onclick=function(){var lc=q.lc.value;q.pfx.value=D.prf.prefixKey.getDefault()
                              model[lc]=[layouts[lc][2].split(''),layouts[lc][3].split('')];updGlyphs()}
     q.lc.onchange=updGlyphs
-    //var updLegend=q.pfx.onchange=q.pfx.onkeyup=function(){$('#lyt_lgnd .pfx2').text($(this).val().slice(-1))}
     q.pfx.onfocus=function(){setTimeout(function(){$(q.pfx).select()},1)}
+    q.legend.onclick=function(){q.popup.hidden=0;q.popup_close.focus()}
+    q.popup_close.onclick=function(){q.popup.hidden=1}
   },
   load:function(){
-    q.lc.value=D.prf.kbdLocale();q.pfx.value=D.prf.prefixKey();/*updLegend();*/model={}
+    q.lc.value=D.prf.kbdLocale();q.pfx.value=D.prf.prefixKey();model={}
     for(var lc in layouts){var l=layouts[lc];model[lc]=[l[2].split(''),l[3].split('')]}
     var pm=D.prf.prefixMaps()
     for(var lc in pm){
