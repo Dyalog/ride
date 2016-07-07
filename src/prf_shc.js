d66425741747a0a8f015343d74072a10ba70032f
Nick <nick@dyalog.com>
2016-07-07 14:34:43 +0100

    Prefs>Shortcuts is 100% jQuery-free

--- src/prf_shc.js
+++ src/prf_shc.js
@@ -15,7 +15,7 @@ D.prf_tabs.shc={
       if(p){getKeystroke(u,function(k){k&&u.insertAdjacentHTML('beforebegin',keyHTML(k));updDups()});return!1}
       var u=e.target.closest('.shc_rst'),p=u&&u.parentNode
       if(p){
-        var tr=u.closest('tr'),c=$(tr).data('code')
+        var tr=u.closest('tr'),c=tr.dataset.code
         for(var i=0;i<D.cmds.length;i++)if(D.cmds[i][0]===c){
           var a=tr.getElementsByClassName('shc_key');for(var j=a.length-1;j>=0;j--)a[j].parentNode.removeChild(a[j])
           tr.querySelector('.shc_add').insertAdjacentHTML('beforebegin',D.cmds[i][2].map(keyHTML).join(''))
@@ -24,12 +24,8 @@ D.prf_tabs.shc={
         return!1
       }
     }
-    q.sc.onkeyup=q.sc.onchange=function(){
-      var a=q.tbl_wr.querySelectorAll('tr'),s=this.value.toLowerCase(),empty=1;q.sc_clr.hidden=!s
-      for(var i=0;i<a.length;i++)empty&=a[i].hidden=a[i].textContent.toLowerCase().indexOf(s)<0
-      q.no_res.hidden=!empty
-    }
-    q.sc_clr.onclick=function(){this.hidden=1;q.sc.value='';$(q.sc).change();q.sc.focus();return!1}
+    q.sc.onkeyup=q.sc.onchange=updSC
+    q.sc_clr.onclick=function(){this.hidden=1;q.sc.value='';updSC();q.sc.focus();return!1}
   },
   load:function(){
     var h=D.prf.keys(),html='<table>',cmds=D.cmds
@@ -40,30 +36,31 @@ D.prf_tabs.shc={
         '<td id=shc_'+c+'>'+(h[c]||d).map(keyHTML).join('')+'<button class=shc_add title="Add shortcut">+</button>'+
         '<td><button class=shc_rst title="Reset to default">↶</button>'
     }
-    document.getElementById('shc_tbl_wr').innerHTML=html+'</table>'
-    updDups();if(q.sc.value){q.sc.value='';$(q.sc).change()}
+    q.tbl_wr.innerHTML=html+'</table>';updDups();if(q.sc.value){q.sc.value='';updSC()}
   },
   validate:function(){var a=q.tbl_wr.getElementsByClassName('shc_dup');if(a.length)return{msg:'Duplicate shortcuts',el:a[0]}},
   save:function(){
-    var h={},cmds=D.cmds
-    for(var i=0;i<cmds.length;i++){
-      var c=cmds[i][0],d=cmds[i][2],a=$('#shc_'+c+' .shc_text').map(function(){return $(this).text()}).toArray()
-      if(JSON.stringify(a)!==JSON.stringify(d))h[c]=a
-    }
+    var h={},cmds=D.cmds,a=q.tbl_wr.querySelectorAll('.shc_text')
+    for(var i=0;i<a.length;i++){var c=a[i].closest('td').id.replace(/^shc_/,'');(h[c]=h[c]||[]).push(a[i].textContent)}
+    for(var i=0;i<cmds.length;i++){var c=cmds[i][0],d=cmds[i][2].slice(0).sort() //d:defaults
+                                   if(h[c]&&JSON.stringify(h[c].sort())===JSON.stringify(d))delete h[c]}
     D.prf.keys(h)
   },
   activate:function(){q.sc.focus()}
 }
-function keyHTML(k){
-  return'<span class=shc_key><span class=shc_text>'+k+'</span><a href=# class=shc_del title="Remove shortcut">×</a></span> '
-}
-function updKeys(x){
-  var h=CodeMirror.keyMap.dyalog={fallthrough:'dyalogDefault'}
-  for(var i=0;i<D.cmds.length;i++){var c=D.cmds[i][0],d=D.cmds[i][2],ks=x[c]||d;for(var j=0;j<ks.length;j++)h[ks[j]]=c}
-}
+function updSC(){var a=q.tbl_wr.querySelectorAll('tr'),s=this.value.toLowerCase(),empty=1;q.sc_clr.hidden=!s
+                 for(var i=0;i<a.length;i++)empty&=a[i].hidden=a[i].textContent.toLowerCase().indexOf(s)<0
+                 q.no_res.hidden=!empty}
+function keyHTML(x){return'<span class=shc_key><span class=shc_text>'+x+'</span>'+
+                                              '<a href=# class=shc_del title="Remove shortcut">×</a></span> '}
+function updKeys(x){var h=CodeMirror.keyMap.dyalog={fallthrough:'dyalogDefault'}
+                    for(var i=0;i<D.cmds.length;i++){var c=D.cmds[i][0],d=D.cmds[i][2],ks=x[c]||d
+                                                     for(var j=0;j<ks.length;j++)h[ks[j]]=c}}
 D.prf.keys(updKeys);updKeys(D.prf.keys())
-function updDups(){var h={} //h:maps keystrokes to jQuery objects
-  $('#shc_tbl_wr .shc_text').each(function(){var $t=$(this),k=$t.text();$t.add(h[k]).toggleClass('shc_dup',!!h[k]);h[k]=$t})
+function updDups(){
+  var a=q.tbl_wr.querySelectorAll('.shc_text'),h={} //h:maps keystrokes to jQuery objects
+  for(var i=0;i<a.length;i++){var k=a[i].textContent
+                              a[i].className=h[k]?(h[k].className='shc_text shc_dup'):'shc_text';h[k]=a[i]}
 }
 function getKeystroke(b,f){ //b:"+" button,f:callback
   var inp=document.createElement('input');inp.placeholder='Enter keystroke...'
