6e1781f46c6632b506c10147eff064f6c84527e4
Nick <nick@dyalog.com>
2016-07-14 12:53:12 +0100

    made chk-hlp-urls.js print number of failures

--- tools/chk-hlp-urls.js
+++ tools/chk-hlp-urls.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
-const http=require('http')
-D={modules:{}};require('../src/hlp');const h=D.hlp
-const a=Object.keys(h).map(k=>h[k]).sort(),o={host:'help.dyalog.com',port:80,path:''}
-const rec=i=>{
-  if(i>=a.length)return
-  o.path=escape(a[i].replace(/^http:\/\/[a-z0-9\.]+\//i,'/').replace(/#.*/,''))
-  http.get(o,r=>{console.log((r.statusCode===200?'   ':r.statusCode)+' '+a[i]);rec(i+1)})
-      .on('error',e=>{console.log(JSON.stringify(e.message)+' '+a[i]);rec(i+1)})
-}
+let total=0,failed=0;D={modules:{}};require('../src/hlp')
+const h=D.hlp,http=require('http'),a=Object.keys(h).map(k=>h[k]).sort(),o={host:'help.dyalog.com',port:80,path:''}
+,rec=i=>{if(i>=a.length){console.log(failed?'failed:'+failed+'/'+total:'ok');return}
+         o.path=escape(a[i].replace(/^http:\/\/[a-z0-9\.]+\//i,'/').replace(/#.*/,''))
+         http.get(o,x=>{console.log((x.statusCode===200?'   ':x.statusCode)+' '+a[i]);rec(i+1)})
+             .on('error',x=>{console.log(JSON.stringify(x.message)+' '+a[i]);rec(i+1)})}
 rec(0)
+
