04c0b7c26f2443dcdf98e56fc7288f770c47efab
Nick <nick@dyalog.com>
2016-07-27 09:30:16 +0100

    updated script that generates "fold" pngs from svg

--- style/img/fold.sh
+++ style/img/fold.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
-rm -f *.png
-for x in open folded cont end; do inkscape _.svg -C -j -e${x}.png -i$x -w16; done
-exiftool -all= -overwrite_original_in_place *.png
+for x in open folded cont end; do
+  rm -f fold_${x}.png
+  inkscape fold.svg -C -j -e fold_${x}.png -i $x -w16
+done
+exiftool -all= -overwrite_original_in_place fold_*.png
