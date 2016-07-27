60d931e520dd16fb6957b40b15ec054bfec8f27c
Nick <nick@dyalog.com>
2016-07-27 09:34:08 +0100

    updated script that generates breakpoint.png

--- style/img/breakpoint.sh
+++ style/img/breakpoint.sh
@@ -2,6 +2,6 @@
 set -e
 for x in breakpoint; do
   rm -f ${x}.png
-  inkscape _.svg -i$x -D -j -e${x}.png
+  inkscape ${x}.svg -i$x -D -j -e${x}.png
   exiftool -all= -overwrite_original_in_place ${x}.png
 done
