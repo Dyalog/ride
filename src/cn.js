6c45f880238c6945b77569e3a9d74464c007491e
Nick <nick@dyalog.com>
2016-08-04 15:18:20 +0100

    when fetching list of interpreters over ssh, don't complain that "exe" is required

--- src/cn.js
+++ src/cn.js
@@ -117,7 +117,7 @@ D.cn=_=>{ //set up Connect page
   q.ssh.onchange=_=>{q.ssh_dtl.hidden=!q.ssh.checked;updExes()}
   q.ssh_user.placeholder=user
   q.fetch.onclick=_=>{
-    if(!validate())return
+    if(!validate($.extend({},sel,{exe:'x'})))return //validate all except "exe"
     q.fetch.disabled=1
     const c=sshExec({host:sel.host,port:+sel.port||22,user:sel.user||user,pass:q.ssh_pass.value,key:q.ssh_key.value},
                     '/bin/ls /opt/mdyalog/*/*/*/mapl /Applications/Dyalog-*/Contents/Resources/Dyalog/mapl 2>/dev/null',
