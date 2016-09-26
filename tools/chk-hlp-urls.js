458cf8d0dc68a4b34608c4498af065089c4b7632
Nick <nick@dyalog.com>
2016-09-26 10:40:11 +0100

    fixed script that checks help urls

--- tools/chk-hlp-urls.js
+++ tools/chk-hlp-urls.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-let total=0,failed=0;D={modules:{}};require('../src/hlp')
+D={modules:{}};require('../src/hlp');let n=0,m=0; //n:total,m:failed
 const h=D.hlp,http=require('http'),a=Object.keys(h).map(k=>h[k]).sort(),o={host:'help.dyalog.com',port:80,path:''}
-,rec=i=>{if(i>=a.length){console.log(failed?'failed:'+failed+'/'+total:'ok');return}
+,rec=i=>{if(i>=a.length){console.log(m?'failed:'+m+'/'+n:'ok');return}
          o.path=escape(a[i].replace(/^http:\/\/[a-z0-9\.]+\//i,'/').replace(/#.*/,''))
-         http.get(o,x=>{console.log((x.statusCode===200?'   ':x.statusCode)+' '+a[i]);rec(i+1)})
-             .on('error',x=>{console.log(JSON.stringify(x.message)+' '+a[i]);rec(i+1)})}
+         http.get(o,x=>{let c=x.statusCode;console.log((c===200?'   ':c)+' '+a[i]);m+=c!==200;rec(i+1)})
+             .on('error',x=>{console.log(JSON.stringify(x.message)+' '+a[i]);m++;rec(i+1)})}
 rec(0)
 
