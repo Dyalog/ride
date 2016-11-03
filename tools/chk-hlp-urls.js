14fbf7a06d27a7c5e198f07b52263ae83c734b78
Nick <nick@dyalog.com>
2016-11-03 13:28:24 +0000

    in generated code, mention which script generates it

--- tools/chk-hlp-urls.js
+++ tools/chk-hlp-urls.js
@@ -2,7 +2,7 @@
 D={modules:{}};require('../src/hlp');let n=0,m=0; //n:total,m:failed
 const h=D.hlp,http=require('http'),a=Object.keys(h).map(k=>h[k]).sort(),o={host:'help.dyalog.com',port:80,path:''}
 ,rec=i=>{if(i>=a.length){console.log(m?'failed:'+m+'/'+n:'ok');return}
-         o.path=escape(a[i].replace(/^http:\/\/[a-z0-9\.]+\//i,'/').replace(/#.*/,''))
+         n++;o.path=escape(a[i].replace(/^http:\/\/[a-z0-9\.]+\//i,'/').replace(/#.*/,''))
          http.get(o,x=>{let c=x.statusCode;console.log((c===200?'   ':c)+' '+a[i]);m+=c!==200;rec(i+1)})
              .on('error',x=>{console.log(JSON.stringify(x.message)+' '+a[i]);m++;rec(i+1)})}
 rec(0)
