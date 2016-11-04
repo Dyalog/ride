a7c6f0f6f210acbb64afbad87e4831fe4201eb61
Nick <nick@dyalog.com>
2016-11-04 13:14:15 +0000

    shorter code in src/fld.js

--- src/fld.js
+++ src/fld.js
@@ -5,7 +5,7 @@
 
 CM.defineOption('foldGutter',false,function(cm,v,o){ //v:new value, o:old value
   if(o&&o!==CM.Init){cm.clearGutter('cm-foldgutter');cm.state.foldGutter=null;for(var k in eh)cm.off(k,eh[k])}
-  if(v){cm.state.foldGutter={from:0,to:0};updInViewport(cm);for(var k in eh)cm.on(k,eh[k])}
+  if(v){cm.state.foldGutter={from:0,to:0};eh.swapDoc(cm);for(var k in eh)cm.on(k,eh[k])}
 })
 function getFold(cm,l){var m=cm.findMarksAt(CM.Pos(l))
                        for(var i=0;i<m.length;i++)if(m[i].__isFold&&m[i].find().from.line===l)return m[i]}
@@ -23,7 +23,7 @@ function updMarkers(cm,a,b){ //a..b:from..to line - ignored, we always update th
   })
 }
 var eh={} //event handlers
-var updInViewport=eh.swapDoc=function(cm){
+eh.swapDoc=function(cm){
   var vp=cm.getViewport(),h=cm.state.foldGutter
   if(h){cm.operation(function(){updMarkers(cm,vp.from,vp.to)});h.from=vp.from;h.to=vp.to}
 }
@@ -31,13 +31,13 @@ eh.gutterClick=function(cm,l,g){ //l:line,g:gutter
   if(cm.state.foldGutter&&g==='cm-foldgutter'){var u=getFold(cm,l);u?u.clear():cm.foldCode(CM.Pos(l,0))}
 }
 eh.change=function(cm){var h=cm.state.foldGutter;if(!h)return
-                       h.from=h.to=0;clearTimeout(h.tid);h.tid=setTimeout(function(){updInViewport(cm)},600)}
+                       h.from=h.to=0;clearTimeout(h.tid);h.tid=setTimeout(function(){eh.swapDoc(cm)},600)}
 eh.viewportChange=function(cm){
   var h=cm.state.foldGutter;if(!h)return
   clearTimeout(h.tid);h.tid=setTimeout(function(){
     var vp=cm.getViewport(), c=h.from===h.to||vp.from-h.to>20||h.from-vp.to>20
-    c?updInViewport(cm):cm.operation(function(){if(vp.from<h.from){updMarkers(cm,vp.from,h.from);h.from=vp.from}
-                                                if(vp.to  >h.to  ){updMarkers(cm,h.to   ,vp.to );h.to  =vp.to  }})
+    c?eh.swapDoc(cm):cm.operation(function(){if(vp.from<h.from){updMarkers(cm,vp.from,h.from);h.from=vp.from}
+                                             if(vp.to  >h.to  ){updMarkers(cm,h.to   ,vp.to );h.to  =vp.to  }})
   },100)
 }
 eh.fold=eh.unfold=function(cm,lh){var h=cm.state.foldGutter;if(h){var l=lh.line;h.from<=l&&l<h.to&&updMarkers(cm,l,l+1)}}
