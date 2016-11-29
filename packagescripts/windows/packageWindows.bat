68ddb340d3e53f55d580333dfc958d888410c936
Jason Rivers <jason@jasonrivers.co.uk>
2016-11-29 09:41:30 +0000

    don't use %WORKSPACE%

--- packagescripts/windows/packageWindows.bat
+++ packagescripts/windows/packageWindows.bat
@@ -4,4 +4,4 @@ set RIDE_BRANCH=master
 
 set CYGWIN_DIR=c:/cygwin
 set CYGWIN=nodosfilewarning
-%CYGWIN_DIR%/bin/bash.exe --login %WORKSPACE%/ride/windows/make.sh
+%CYGWIN_DIR%/bin/bash.exe --login ./make.sh
