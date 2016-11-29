c19fef350f4999f8fd4df45176a1e80e4539cf1b
Jason Rivers <jason@jasonrivers.co.uk>
2016-11-29 09:51:59 +0000

    working on windows paths through Jenkins/Cygwin

--- packagescripts/windows/packageWindows.bat
+++ packagescripts/windows/packageWindows.bat
@@ -1,7 +1,9 @@
 call "c:/Program files (x86)/Microsoft Visual Studio 8/VC/vcvarsall.bat"
 
+set JENKINSDIR=%CD%
+
 set RIDE_BRANCH=master
 
 set CYGWIN_DIR=c:/cygwin
 set CYGWIN=nodosfilewarning
-%CYGWIN_DIR%/bin/bash.exe --login ./make.sh
+%CYGWIN_DIR%/bin/bash.exe --login %JENKINSDIR%/make.sh
