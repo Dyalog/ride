c805e939b768b643aefb9506a4d933c8964a9fa5
Nick <nick@dyalog.com>
2016-11-29 14:14:51 +0000

    new tree for the wse

---
+++ src/bonsai.js
@@ -0,0 +1,32 @@
+function Bonsai(e,o){ //e:dom element, o:options={children:function(id,callback){...}, click:function(path){...}}
+  var bt=this //bonsai tree
+  bt.nodes={0:{id:0,text:'',expandable:1,icon:''}}
+  e.innerHTML=bt.render(bt.nodes[0])
+  e.onmousedown=function(event){
+    if(event.target.matches('.bt_node_expand')){
+      var a=event.target, node=bt.nodes[a.parentNode.dataset.id];if(!node||!node.expandable)return
+      node.expanded=1-!!node.expanded; a.textContent='+-'[+!!node.expanded]
+      node.expanded&&o.children(node.id,function(children){
+        node.children=children
+        children.forEach(function(c){bt.nodes[c.id]=c})
+        a.parentNode.outerHTML=bt.render(node)
+      })
+      a.parentNode.className=node.expanded?'':'bt_collapsed'
+      return!1
+    }else if(o.click&&event.target.matches('.bt_text')){
+      var path=[], div=event.target.parentNode
+      while(div!==e){path.unshift(bt.nodes[div.dataset.id]);div=div.parentNode}
+      o.click(path);return!1
+    }
+  }
+}
+Bonsai.prototype={
+  render:function(node){
+    return'<div data-id="'+node.id+'">'+
+            (node.expandable?'<a class=bt_node_expand>'+'+-'[+!!node.expanded]+'</a>'
+                            :'<a class=bt_node_indent></a>')+
+            '<span data-id='+node.id+' class="bt_icon_'+node.icon+' bt_text">'+node.text+'</span>'+
+            (node.expanded?node.children.map(this.render).join(''):'')+
+          '</div>'
+  }
+}
--- src/wse.js
+++ src/wse.js
@@ -2,23 +2,20 @@
 'use strict'
 D.WSE=function(){
   var pending=this.pending={};this.dom=I.wse;this.dom.hidden=0
-  this.dom.onclick=function(x){var t=x.target.closest('.jstree-anchor');if(!t)return
-                               D.send('Edit',{win:0,pos:0,text:$e.jstree('get_path',t,'.')})
-                               ;/^wse_leaf_/.test(t.id)||$e.jstree('refresh_node',t)}
-  var $e=this.$e=$(this.dom).jstree({plugins:[],core:{animation:true,check_callback:true,data:function(x,f){
-    var i=x.id==='#'?0:+x.id.replace(/\D+/g,'')
-    pending[i]=f.bind(this);D.send('TreeList',{nodeId:i}) //ask interpreter for info about i's children
-  }}})
+  var bt=new Bonsai(this.dom,{
+    children:function(id,callback){pending[id]=callback.bind(this);D.send('TreeList',{nodeId:id})},
+    click:function(path){D.send('Edit',{win:0,pos:0,text:path.slice(1).map(function(x){return x.text}).join('.')})}
+  })
 }
 D.WSE.prototype={
   replyTreeList:function(x){ //handle response from interpreter
     var f=this.pending[x.nodeId];if(!f)return
     f((x.nodeIds||[]).map(function(c,i){
       //x.classes uses constants from http://help.dyalog.com/15.0/Content/Language/System%20Functions/nc.htm
-      var t=Math.floor(x.classes[i]);return{text:x.names[i],children:!!c,id:'wse_'+(c||('leaf_'+x.nodeId+'_'+i)),
-                                            icon:c?'':2<=t&&t<=4?'style/img/wse_nc'+t+'.png':'style/img/tb_ED.png'}
+      var t=Math.floor(x.classes[i]);return{id:c||('leaf_'+x.nodeId+'_'+i),
+                                            text:x.names[i],expandable:!!c,icon:t}
     }))
     delete this.pending[x.nodeId]
   },
-  refresh:function(){this.$e.jstree('refresh')}
+  refresh:function(){/*todo*/}
 }
