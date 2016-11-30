decac7a5e9ab2de4418cf494fbd0d56bee26b1dc
Jason Rivers <jason@jasonrivers.co.uk>
2016-11-30 09:52:26 +0000

    missing / from file path

--- packagescripts/osx/packageOSX.sh
+++ packagescripts/osx/packageOSX.sh
@@ -44,7 +44,7 @@ PLISTValue "$PLISTFILE" "CFBundleDisplayName" "$APPNAME"
 PLISTValue "$PLISTFILE" "CFBundleName" "$APPNAME"
 
 mkdir -p ./OSX-Packing/
-rsync -a --copy-links ${RIDEDIR}/${RIDEAPPDIRNAME} ./OSX-Packing/${APPNAME}.app
+rsync -a --copy-links ${RIDEDIR}/${RIDEAPPDIRNAME}/ ./OSX-Packing/${APPNAME}.app
 
 cd OSX-Packing
 
