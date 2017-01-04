986b91701b7c601b7c7d4e56fc8e27fe4e204797
Jason Rivers <jason@jasonrivers.co.uk>
2017-01-04 15:16:57 +0000

    use real directories on network share

--- copyinstallers.sh
+++ copyinstallers.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -x -e -o pipefail
 
-CHECKPR=${JOB_NAME#*/*/}
+JOB_NAME=${JOB_NAME#*/} ## remove folder from the job_name
+
+CHECKPR=${JOB_NAME#*/}
 if [ "${CHECKPR:0:2}" = "PR" ]; then
         echo "skipping copying files for pull requests"
         exit 0
@@ -10,9 +12,9 @@ fi
 
 GIT_BRANCH=`git symbolic-ref --short HEAD`
 
-## Sheck /devt is mounted
+## Check /devt is mounted
 
 mountpoint /devt; echo Devt is mouted: good
-RIDEDIR=/devt/builds/JR-Test/${JOB_NAME}/${GIT_BRANCH}-${BUILD_NUMBER}/latest
+RIDEDIR=/devt/builds/${JOB_NAME}/latest
 
 cp -vR ship $RIDEDIR
--- publish.sh
+++ publish.sh
@@ -24,7 +24,7 @@ CURRENTBRANCH=${GIT_BRANCH#*/}
 
 umask 002 # user and group can do everything, others can only read and execute
 mountpoint /devt; echo Devt is mounted: good # make sure it's mounted
-r=/devt/builds/JR-Test/${JOB_NAME}
+r=/devt/builds/${JOB_NAME}
 d=${BUILD_NUMBER}
 for suffix in '' {a..z}; do if [ ! -e $r/$d$suffix ]; then d=$d$suffix; break; fi; done
 mkdir -p $r/$d
