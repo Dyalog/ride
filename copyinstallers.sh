b78a0da19cb390d01ff636958c7b22f853ea4755
Jason Rivers <jason@jasonrivers.co.uk>
2017-01-04 15:01:55 +0000

    don't copy installer images for PRs

--- copyinstallers.sh
+++ copyinstallers.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -x -e -o pipefail
 
+CHECKPR=${JOB_NAME#*/*/}
+if [ "${CHECKPR:0:2}" = "PR" ]; then
+        echo "skipping copying files for pull requests"
+        exit 0
+fi
+
+
 GIT_BRANCH=`git symbolic-ref --short HEAD`
 
 ## Sheck /devt is mounted
