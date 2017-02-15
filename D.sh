a3c351855a26e2ce57e1782ea971c47b18cb9ea1
Jay Foad <jay.foad@gmail.com>
2017-02-15 12:09:40 +0000

    Better Windows application icon

--- D.sh
+++ D.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm D.icns
-
-for S in 16 32 48 128 256 512; do
-convert D.png -resize ${S}x${S} icns_${S}px.png
-
+for S in 16 24 32 48 128 256 512; do
+    convert D.png -resize ${S}x${S} D$S.png
 done
 
-png2icns D.icns icns_*px.png
+# http://stackoverflow.com/questions/3236115/which-icon-sizes-should-my-windows-applications-icon-include
+convert D{16,24,32,48,256}.png D.ico
 
-rm icns_*px.png
+png2icns D.icns D{16,32,48,128,256,512}.png > /dev/null
+
+rm D[0-9]*.png
