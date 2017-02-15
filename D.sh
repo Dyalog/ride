9a0715c16a31020d7c0afb392159e3cb96c04170
Jay Foad <jay.foad@gmail.com>
2017-02-15 12:47:17 +0000

    Fix Windows application icon

--- D.sh
+++ D.sh
@@ -5,7 +5,7 @@ for S in 16 24 32 48 128 256 512; do
 done
 
 # http://stackoverflow.com/questions/3236115/which-icon-sizes-should-my-windows-applications-icon-include
-convert D{16,24,32,48,256}.png D.ico
+convert D{256,48,32,24,16}.png D.ico
 
 png2icns D.icns D{16,32,48,128,256,512}.png > /dev/null
 
