3f7062a22227b7a3cbcd9bcd388b0d7248a0b7c2
flouc001 <flouc001@gmail.com>
2017-03-03 11:00:48 +0000

    improved debug window styling and added HTML encoding

--- src/dbg.js
+++ src/dbg.js
@@ -8,19 +8,32 @@ D.DBG=function(){
     click_handler:function(e){
       D.send('SetSIStack',{stack:e.target.innerHTML})
     },
-    no_item_message:'&lt;no stack&gt;'
+    no_item_message:'&lt;no stack&gt;',
+    render:function(array){
+      var encodeHTML=function(i){return $('<div/>').text(i).html()}
+      if (array.length>0){
+        this.dom.innerHTML=array.sort(function(p,n){return p.tid-n.tid}).map((function(i){
+          return "<div class=\""+this.item_class+" ctl_listview_item\">"
+              +"<span class=stack_desc>" +encodeHTML(i.description)+"</span>"
+            +"</div>"
+        }).bind(this)).join('')
+      }else{
+        this.dom.innerHTML="<div>"+this.no_item_message+"</div>"
+      }
+    }
   })
   dbg.threads=new D.ListView('threads',{
     item_class:'threaditem',
     click_handler:function(e){D.send('SetThread',{stack:e.target.innerHTML})},
     render:function(array){
+      var encodeHTML=function(i){return $('<div/>').text(i).html()}
       if (array.length>0){
         this.dom.innerHTML=array.sort(function(p,n){return p.tid-n.tid}).map((function(i){
           return "<div class=\""+this.item_class+" ctl_listview_item\">"
-              +"<span class=thread_desc>" +i.description+"</span>"
-              +"<span class=thread_state>"+i.state      +"</span>"
-              +"<span class=thread_tid>"  +i.tid        +"</span>"
-              +"<span class=thread_flags>"+i.flags      +"</span>"
+              +"<span class=thread_desc>" +encodeHTML(i.description)+"</span>"
+              +"<span class=thread_state>"+encodeHTML(i.state)+"</span>"
+              +"<span class=thread_tid>"  +encodeHTML(i.tid)+"</span>"
+              +"<span class=thread_flags>"+encodeHTML(i.flags)+"</span>"
             +"</div>"
         }).bind(this)).join('')
       }
@@ -43,6 +56,5 @@ D.DBG=function(){
 D.DBG.prototype={
   TR:function(){D.send('GetThreads',{})},
   RM:function(){D.send('Continue'      ,{win:this.threads.selected.id})},
-  MA:function(){D.send('RestartThreads',{win:this.threads.selected.id})},
-  
+  MA:function(){D.send('RestartThreads',{win:this.threads.selected.id})}
 }
