ba4314bf77178d5b35926d14116d9275a92d532d
flouc001 <flouc001@gmail.com>
2017-05-18 16:05:22 +0100

    colour changes to wse

--- src/bonsai.js
+++ src/bonsai.js
@@ -12,7 +12,7 @@ function Bonsai(e,o){ //e:dom element, o:options={children:function(id,callback)
   }
   var toggleNode=function(tgt){
     var a=tgt, node=bt.nodes[a.parentNode.dataset.id];if(!node||!node.expandable)return
-    node.expanded=1-!!node.expanded; a.textContent='⊞⊟'[+!!node.expanded]
+    node.expanded=1-!!node.expanded; a.textContent='+-'[+!!node.expanded]
     node.expanded&&o.children(node.id,function(children){
       node.children=children
       children.forEach(function(c){bt.nodes[c.id]=c})
@@ -65,7 +65,7 @@ Bonsai.prototype={
   render:function(node,selected){
     var bt=this
     return'<div data-id="'+node.id+'">'+
-            (node.expandable?'<a class=bt_node_expand>'+'⊞⊟'[+!!node.expanded]+'</a>'
+            (node.expandable?'<a class=bt_node_expand>'+'+-'[+!!node.expanded]+'</a>'
                             :'<a class=bt_node_indent></a>')+
             '<span tabIndex=-1 data-id='+node.id+' class="bt_icon_'+node.icon+' bt_text '+(selected?'selected':'')+'">'+
               node.text+
