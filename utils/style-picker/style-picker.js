0e23fbd19b61bdf64aaa02ef7af6056b4c8ec8b6
flouc001 <flouc001@gmail.com>
2017-10-06 13:32:07 +0100

    a small utility for themes

---
+++ utils/style-picker/style-picker.js
@@ -0,0 +1,9 @@
+const electron=require('electron');
+
+var { app }=electron;
+app.on('ready',(event,launch_info)=>{
+    var elw = new electron.BrowserWindow({});
+    elw.loadURL(`file://${__dirname}/index.html`);
+    elw.webContents.openDevTools();
+    elw.show();
+})
