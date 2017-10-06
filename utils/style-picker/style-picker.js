af79d0827ee6cbd0bd1f513f1744c27eda53c456
flouc001 <flouc001@gmail.com>
2017-10-06 16:12:21 +0100

    cleaned up styler application

--- utils/style-picker/style-picker.js
+++ utils/style-picker/style-picker.js
@@ -5,5 +5,6 @@ app.on('ready',(event,launch_info)=>{
     var elw = new electron.BrowserWindow({});
     elw.loadURL(`file://${__dirname}/index.html`);
     elw.webContents.openDevTools();
+    elw.setMenu(null);
     elw.show();
 })
