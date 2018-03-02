e352f5f3823c01171bbf3dbec00efc6a957b0a20
Gilgamesh Athoraya <e9gille@gmail.com>
2018-03-02 09:08:44 +0100

    lint menu

--- src/menu.js
+++ src/menu.js
@@ -1,31 +1,55 @@
-//two menu implementations -- one native and one in html
-'use strict'
-D.installMenu=function(x){
-  if(D.el){
-    var pk=D.prf.keys(), k={} // k:a map of all keyboard shortcuts
-    for(var i=0;i<D.cmds.length;i++){var c=D.cmds[i][0],d=D.cmds[i][2];k[c]=(pk[c]||d)[0]} // c:code, d:defaults
-    var render=function(x){
-      if(x['']==='-')return new D.el.MenuItem({type:'separator'})
-      var h={label:x[''],click:x.action,accelerator:x.cmd&&k[x.cmd]?k[x.cmd].replace(/-(.)/g,'+$1'):null}
-      if(x.group){
-        h.type='radio';h.checked=!!x.checked
-      }else if(x.checkBoxPref){
-        h.type='checkbox';h.checked=!!x.checkBoxPref()
-        if(x.action)h.click=function(){x.action(mi.checked)}
-        x.checkBoxPref(function(v){mi.checked=!!v})
+// two menu implementations -- one native and one in html
+D.installMenu = function Menu(mx) {
+  if (D.el) {
+    const pk = D.prf.keys();
+    const k = {}; // k:a map of all keyboard shortcuts
+    for (let i = 0; i < D.cmds.length; i++) {
+      const [c,, d] = D.cmds[i];
+      [k[c]] = (pk[c] || d);
+    } // c:code, d:defaults
+    const render = (x) => {
+      let mi;
+      if (x[''] === '-') return new D.el.MenuItem({ type: 'separator' });
+      const h = {
+        label: x[''],
+        click: x.action,
+        accelerator: x.cmd && k[x.cmd] ? k[x.cmd].replace(/-(.)/g, '+$1') : null,
+      };
+      if (x.group) {
+        h.type = 'radio';
+        h.checked = !!x.checked;
+      } else if (x.checkBoxPref) {
+        h.type = 'checkbox';
+        h.checked = !!x.checkBoxPref();
+        if (x.action) h.click = () => { x.action(mi.checked); };
+        x.checkBoxPref((v) => { mi.checked = !!v; });
       }
-      const roles={'undo':1,'redo':1,
-                   'cut':1,'copy':1,'paste':1,'selectall':1,
-                   'togglefullscreen':1,
-                   'window':1,'help':1}
-      var r=x[''].replace(/[& ]/g,'').toLowerCase();if(r in roles)h.role=r
-      if(x.items){h.submenu=new D.el.Menu;x.items.forEach(function(y){h.submenu.append(render(y))})}
-      var mi=new D.el.MenuItem(h);return mi
-    }
-    var m=new D.el.Menu;x.forEach(function(y){m.append(render(y))})
-    if(D.mac)D.el.Menu.setApplicationMenu(m);else D.elw.setMenu(m)
-  }else{
-    var arg=x
+      const roles = {
+        undo: 1,
+        redo: 1,
+        cut: 1,
+        copy: 1,
+        paste: 1,
+        selectall: 1,
+        togglefullscreen: 1,
+        window: 1,
+        help: 1,
+      };
+      const r = x[''].replace(/[& ]/g, '').toLowerCase();
+      if (r in roles) h.role = r;
+      if (x.items) {
+        h.submenu = new D.el.Menu();
+        x.items.forEach((y) => { h.submenu.append(render(y)); });
+      }
+      mi = new D.el.MenuItem(h);
+      return mi;
+    };
+    const m = new D.el.Menu();
+    mx.forEach((y) => { m.append(render(y)); });
+    if (D.mac) D.el.Menu.setApplicationMenu(m);
+    else D.elw.setMenu(m);
+  } else {
+    const arg = mx;
     // DOM structure:
     // ┌.menu───────────────────────────────────────────┐
     // │┌div.m-sub────────────────────────────────┐     │
@@ -39,74 +63,114 @@ D.installMenu=function(x){
     // │└─────────────────────────────────────────┘     │
     // └────────────────────────────────────────────────┘
     // Top-level ".m-opener"-s also have class ".m-top"
-    var render=function(x){
-      if(!x)return
-      if(x['']==='-')return $('<hr>')
-      var acc,name=x[''].replace(/&(.)/g,function(_,k){return acc||k==='&'?k:'<u>'+(acc=k)+'</u>'}) // acc:access key
-      var $a=$('<a href=#><span>'+name+'</span></a>')
-      x.cmd&&$a.append('<span class=m-shc data-cmd='+x.cmd+'>')
-      if(x.group){
-        $a.addClass('m-group-'+x.group).toggleClass('m-checked',!!x.checked).on('mousedown mouseup click',function(e){
-          $(this).closest('.menu').find('.m-group-'+x.group).removeClass('m-checked');$(this).addClass('m-checked')
-          mFocus(null);x.action&&x.action();return!1
-        })
-      }else if(x.checkBoxPref){
-        x.checkBoxPref(function(v){$a.toggleClass('m-checked',!!v)})
-        $a.toggleClass('m-checked',!!x.checkBoxPref()).on('mousedown mouseup click',function(e){
-          mFocus(null);x.action&&x.action($(this).hasClass('m-checked'));return!1
-        })
-      }else{
-        x.action&&$a.on('mousedown mouseup click',function(e){mFocus(null);x.action();return!1})
+    let $o; // original focused element
+    let $m;
+    const mFocus = (anchor) => {
+      $m.find('.m-open').removeClass('m-open');
+      if (!anchor) { $o && $o.focus(); $o = null; return; }
+      $o || ($o = $(':focus'));
+      const $a = $(anchor);
+      $a.parentsUntil('.menu').addClass('m-open');
+      $a.is('.m-top') ? $a.closest('.m-sub').find('a').eq(1).focus() : $a.focus();
+    };
+    const render = (x) => {
+      if (!x) return null;
+      if (x[''] === '-') return $('<hr>');
+      let acc;
+      const name = x[''].replace(/&(.)/g, (_, k) => {
+        const r = acc || k === '&' ? k : `<u>${acc = k}</u>`;
+        return r;
+      }); // acc:access key
+      const $a = $(`<a href=#><span>${name}</span></a>`);
+      x.cmd && $a.append(`<span class=m-shc data-cmd=${x.cmd}>`);
+      if (x.group) {
+        $a.addClass(`m-group-${x.group}`)
+          .toggleClass('m-checked', !!x.checked)
+          .on('mousedown mouseup click', (e) => {
+            const $e = $(e.currentTarget);
+            $e.closest('.menu').find(`.m-group-${x.group}`).removeClass('m-checked');
+            $e.addClass('m-checked');
+            mFocus(null);
+            x.action && x.action();
+            return !1;
+          });
+      } else if (x.checkBoxPref) {
+        x.checkBoxPref((v) => { $a.toggleClass('m-checked', !!v); });
+        $a.toggleClass('m-checked', !!x.checkBoxPref())
+          .on('mousedown mouseup click', (e) => {
+            mFocus(null);
+            x.action && x.action($(e.currentTarget).hasClass('m-checked'));
+            return !1;
+          });
+      } else {
+        x.action && $a.on('mousedown mouseup click', () => { mFocus(null); x.action(); return !1; });
       }
-      if(!x.items)return $a
-      var $b=$('<div class=m-box>')
-      return $('<div class=m-sub>').append($a.addClass('m-opener'),$b.append.apply($b,x.items.map(render)))
-    }
-    var $o // original focused element
-    var mFocus=function(anchor){
-      $m.find('.m-open').removeClass('m-open');if(!anchor){$o&&$o.focus();$o=null;return}
-      $o||($o=$(':focus'));var $a=$(anchor);$a.parentsUntil('.menu').addClass('m-open')
-      $a.is('.m-top')?$a.closest('.m-sub').find('a').eq(1).focus():$a.focus()
-    }
-    var leftRight=function(d,$e){ // d: +1 or -1, $e: target element
-      if(d>0&&$e.is('.m-opener')){
-        mFocus($e.next('.m-box').find('a').first())
-      }else if(d<0&&!$e.is('.m-opener')&&$e.parents('.m-sub').length>1){
-        mFocus($e.closest('.m-sub').find('.m-opener').first())
-      }else{
-        var $t=$m.children(),n=$t.length,i=$e.parentsUntil('.menu').last().index();mFocus($t.eq((i+d+n)%n).find('a').eq(1))
+      if (!x.items) return $a;
+      const $b = $('<div class=m-box>');
+      return $('<div class=m-sub>')
+        .append($a.addClass('m-opener'), $b.append(...x.items.map(render)));
+    };
+    const leftRight = (d, $e) => { // d: +1 or -1, $e: target element
+      if (d > 0 && $e.is('.m-opener')) {
+        mFocus($e.next('.m-box').find('a').first());
+      } else if (d < 0 && !$e.is('.m-opener') && $e.parents('.m-sub').length > 1) {
+        mFocus($e.closest('.m-sub').find('.m-opener').first());
+      } else {
+        const $t = $m.children();
+        const n = $t.length;
+        const i = $e.parentsUntil('.menu').last().index();
+        mFocus($t.eq((i + d + n) % n).find('a').eq(1));
       }
-      return!1
-    }
-    var upDown=function(d,$e){ // d: +1 or -1, $e: target element
-      if($e.is('.m-top')){
-        mFocus($e.parent().find(':not(hr)').eq(1))
-      }else{
-        var $s=$e.closest('.m-box').children(':not(hr)'),i=$s.index($e),n=$s.length,$f=$s.eq((i+d+n)%n)
-        mFocus($f.is('a')?$f:$f.find('a').first())
+      return !1;
+    };
+    const upDown = (d, $e) => { // d: +1 or -1, $e: target element
+      if ($e.is('.m-top')) {
+        mFocus($e.parent().find(':not(hr)').eq(1));
+      } else {
+        const $s = $e.closest('.m-box').children(':not(hr)');
+        const i = $s.index($e);
+        const n = $s.length;
+        const $f = $s.eq((i + d + n) % n);
+        mFocus($f.is('a') ? $f : $f.find('a').first());
       }
-      return!1
-    }
-    var $m=$('<div class=menu>').prependTo('body').empty().addClass('menu').append(arg.map(render))
-    $m.find('>.m-sub>.m-opener').addClass('m-top')
-    $m.on('mouseover','a',function(){$(this).closest('.menu').children().is('.m-open')&&mFocus(this)})
-      .on('mousedown click','a',function(e){
-        mFocus($(this).parentsUntil('.menu').last().is('.m-open')&&e.type==='mousedown'?null:this);return!1
-      })
-      .keydown(function(e){
-        switch(CM.keyNames[e.which]){
-          case'Left' :leftRight(-1,$(e.target));break
-          case'Right':leftRight( 1,$(e.target));break
-          case'Up'   :upDown   (-1,$(e.target));break
-          case'Down' :upDown   ( 1,$(e.target));break
-          case'Esc':case'F10':mFocus(null);return!1
-        }
-      })
-    $(document).mousedown(function(e){$(e.target).closest('.menu').length||mFocus(null)})
-    var updShcs=function(h){
-      var k={};for(var i=0;i<D.cmds.length;i++){var c=D.cmds[i][0],d=D.cmds[i][2];k[c]=(h[c]||d)[0]} // c:code, d:defaults
-      $('.m-shc').each(function(){$(this).text(k[$(this).data('cmd')]||'')})
-    }
-    updShcs(D.prf.keys());D.prf.keys(updShcs)
+      return !1;
+    };
+    $m = $('<div class=menu>').prependTo('body').empty()
+      .addClass('menu')
+      .append(arg.map(render));
+    $m.find('>.m-sub>.m-opener').addClass('m-top');
+    $m.on('mouseover', 'a', (e) => {
+      const t = e.currentTarget;
+      $(t).closest('.menu').children().is('.m-open') && mFocus(t);
+    });
+    $m.on('mousedown click', 'a', (e) => {
+      const t = e.currentTarget;
+      mFocus($(t).parentsUntil('.menu').last().is('.m-open') && e.type === 'mousedown' ? null : t);
+      return !1;
+    });
+    $m.keydown((e) => {
+      switch (CM.keyNames[e.which]) {
+        case 'Left': leftRight(-1, $(e.target)); break;
+        case 'Right': leftRight(1, $(e.target)); break;
+        case 'Up': upDown(-1, $(e.target)); break;
+        case 'Down': upDown(1, $(e.target)); break;
+        case 'Esc': case 'F10': mFocus(null); return !1;
+        default:
+      }
+      return !0;
+    });
+    $(document).mousedown((e) => { $(e.target).closest('.menu').length || mFocus(null); });
+    const updShcs = (h) => {
+      const k = {};
+      for (let i = 0; i < D.cmds.length; i++) {
+        const [c,, d] = D.cmds[i];
+        [k[c]] = (h[c] || d);
+      } // c:code, d:defaults
+      $('.m-shc').each((e) => {
+        const $e = $(e.currentTarget);
+        $e.text(k[$e.data('cmd')] || '');
+      });
+    };
+    updShcs(D.prf.keys()); D.prf.keys(updShcs);
   }
-}
+};
