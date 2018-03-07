01cbbd9c703a37b0484f59117e158410f856ad50
Gilgamesh Athoraya <e9gille@gmail.com>
2018-03-07 21:33:33 +0100

    ED from session unicode issue

--- src/util.js
+++ src/util.js
@@ -1,70 +1,102 @@
-//a kitchen sink for small generic functions and jQuery plugins
+// a kitchen sink for small generic functions and jQuery plugins
 'use strict'
-var zCtr=100 //css z-index counter
-D.util={
-  dict:function(x){var r={};for(var i=0;i<x.length;i++)r[x[i][0]]=x[i][1];return r}, //dictionary from key-value pairs
-  ESC:{'<':'&lt;','>':'&gt;','&':'&amp;',"'":'&apos;','"':'&quot;'},
-  esc:function(s){return s.replace(/[<>&'"]/g,function(x){return D.util.ESC[x]})},
-  cmOnDblClick:function(cm,f){
-    //CodeMirror supports 'dblclick' events but they are unreliable and seem to require rather a short time between the
-    //two clicks. So, let's track clicks manually:
-    var t=0,x=0,y=0 //last click's timestamp and coordinates
-    cm.on('mousedown',function(cm,e){
-      e.timeStamp-t<400&&Math.abs(x-e.clientX)+Math.abs(y-e.clientY)<10&&
-        !$(e.target).closest('.CodeMirror-gutter-wrapper').length&&f(e)
-      t=e.timeStamp;x=e.clientX;y=e.clientY
-    })
-  },
-  dlg:function(d,o){o=o||{} //.dlg(d) shows an element d in a movable dom-only dialog
-    d.style.zIndex=zCtr++;d.hidden=0
-    d.style.left=(0|(innerWidth -(o.w||d.clientWidth ))/2)+'px';if(o.w)d.style.width =o.w+'px'
-    d.style.top =(0|(innerHeight-(o.h||d.clientHeight))/2)+'px';if(o.h)d.style.height=o.h+'px'
-    if(d.__dlg)return;d.__dlg=1
-    var close_button=d.querySelector('.dlg_close');
-    if(close_button) $(d).on('keydown',function(e){
-      if(e.which===27&&!e.ctrlKey&&!e.shiftKey&&!e.altKey&&!e.metaKey){
-          close_button.click();return!1;}})
-    o=null
-    d.onmousedown=function(){d.style.zIndex=zCtr++}
-    d.onclick=function(e){if(e.target.className==='dlg_close'){d.hidden=1;return!1}}
-    var t=d.querySelector('.dlg_title')
-    if(t){
-      var dx,dy //dx,dy:dialog position corrected for mouse
-      t.onmousedown=function(e){
-        if(e.target.closest('.dlg_no_drag'))return
-        dx=d.offsetLeft-e.clientX;dy=d.offsetTop-e.clientY
-        t.style.cursor='move';
-        $(document).on('mousemove',move);
-        e.preventDefault();return!1
-      }
-      t.onmouseup=function(e){
-        $(document).off('mousemove',move);
-        t.style.cursor=''}
-      var move=function(e){d.style.left=(dx+e.clientX)+'px'
-                           d.style.top =(dy+e.clientY)+'px'
-                           e.preventDefault();return!1}
+let zCtr = 100; // css z-index counter
+D.util = {
+  dict(x) {
+    const r = {};
+    for (let i = 0; i < x.length; i++) r[x[i][0]] = x[i][1];
+    return r;
+  }, // dictionary from key-value pairs
+  ESC: { '<': '&lt;', '>': '&gt;', '&': '&amp;', "'": '&apos;', '"': '&quot;' },
+  esc(s) { return s.replace(/[<>&'"]/g, x => D.util.ESC[x]); },
+  dlg(d, opts) {
+    let o = opts || {}; // .dlg(d) shows an element d in a movable dom-only dialog
+    d.style.zIndex = zCtr++;
+    d.hidden = 0;
+    d.style.left = `${(0 | (innerWidth - (o.w || d.clientWidth )) / 2)}px`;
+    if (o.w) d.style.width = `${o.w}px`;
+    d.style.top = `${(0 | (innerHeight - (o.h || d.clientHeight)) / 2)}px`;
+    if (o.h) d.style.height = `${o.h}px`;
+    if (d.__dlg) return;
+    d.__dlg = 1;
+    const closeButton = d.querySelector('.dlg_close');
+    if (closeButton) {
+      $(d).on('keydown', (e) => {
+        if (e.which === 27 && !e.ctrlKey && !e.shiftKey && !e.altKey && !e.metaKey) {
+          closeButton.click();
+          return !1;
+        }
+        return !0;
+      });
+    }
+    o = null;
+    d.onmousedown = () => { d.style.zIndex = zCtr++; };
+    d.onclick = (e) => {
+      if (e.target.className === 'dlg_close') { d.hidden = 1; return !1; }
+      return !0;
+    };
+    const t = d.querySelector('.dlg_title');
+    if (t) {
+      let dx;
+      let dy; // dx,dy:dialog position corrected for mouse
+      const move = (e) => {
+        d.style.left = `${dx + e.clientX}px`;
+        d.style.top = `${dy + e.clientY}px`;
+        e.preventDefault();
+        return !1;
+      };
+      t.onmousedown = (e) => {
+        if (e.target.closest('.dlg_no_drag')) return !0;
+        dx = d.offsetLeft - e.clientX;
+        dy = d.offsetTop - e.clientY;
+        t.style.cursor = 'move';
+        $(document).on('mousemove', move);
+        e.preventDefault();
+        return !1;
+      };
+      t.onmouseup = () => {
+        $(document).off('mousemove', move);
+        t.style.cursor = '';
+      };
     }
   },
-  elastic:function(inp){ //make an <input> stretch when you type long text in it
-    var m=inp.dataset.minSize
-    if(!m){var f=function(){D.util.elastic(inp)};$(inp).on('keyup keypress change',f);
-           inp.dataset.minSize=m=+inp.size||1}
-    inp.size=Math.max(m,inp.value.length+1)
+  ucLength(s) {
+    return node_require('punycode').ucs2.decode(s).length;
   },
-  insert:function(x,y){ //replace selection in an <input> or <textarea> x with the string y
-    if(!x||(x.nodeName!=='INPUT'&&x.nodeName!=='TEXTAREA')||x.readOnly||x.disabled)return
-    var i=x.selectionStart,j=x.selectionEnd,v=x.value
-    if(i!=null&&j!=null){x.value=v.slice(0,i)+y+v.slice(j);x.selectionStart=x.selectionEnd=i+y.length}
-  }
-}
-$.alert=function(m,t,f){ //m:message,t:title,f:callback
-  D.el?D.el.dialog.showMessageBox(D.elw,{message:m,title:t,buttons:['OK']}):alert(m);f&&f()
-}
-$.err=function(m,t,f){
-  if(typeof t==='function'){f=t;t=''}
-  t=t||'Error';D.el?D.el.dialog.showMessageBox(D.elw,{type:'error',message:m,title:t,buttons:['OK']}):alert(m);f&&f()
-}
-$.confirm=function(m,t,f){
-  f(D.el?1-D.el.dialog.showMessageBox(D.elw,{message:m,title:t,type:'question',buttons:['Yes','No'],cancelId:1})
-        :+confirm(m))
-}
+  elastic(inp) { // make an <input> stretch when you type long text in it
+    let m = inp.dataset.minSize;
+    if (!m) {
+      const f = () => { D.util.elastic(inp); };
+      $(inp).on('keyup keypress change', f);
+      m = +inp.size || 1;
+      inp.dataset.minSize = m;
+    }
+    inp.size = Math.max(m, inp.value.length + 1);
+  },
+  insert(x, y) { // replace selection in an <input> or <textarea> x with the string y
+    if (!x || (x.nodeName !== 'INPUT' && x.nodeName !== 'TEXTAREA') || x.readOnly || x.disabled) return;
+    const i = x.selectionStart;
+    const j = x.selectionEnd;
+    const v = x.value;
+    if (i != null && j != null) {
+      x.value = v.slice(0, i) + y + v.slice(j);
+      const d = i + y.length;
+      x.selectionStart = d;
+      x.selectionEnd = d;
+    }
+  },
+};
+$.alert = (m, t, f) => { // m:message,t:title,f:callback
+  D.el ? D.el.dialog.showMessageBox(D.elw, { message: m, title: t, buttons: ['OK'] }) : alert(m);
+  f && f();
+};
+$.err = (m, t, f) => {
+  if (typeof t === 'function') { f = t; t = ''; }
+  t = t || 'Error';
+  D.el ? D.el.dialog.showMessageBox(D.elw, { type: 'error', message: m, title: t, buttons: ['OK'] }) : alert(m);
+  f && f();
+};
+$.confirm = (m, t, f) => {
+  f(D.el ? 1 - D.el.dialog.showMessageBox(D.elw, { message: m, title: t, type: 'question', buttons: ['Yes', 'No'], cancelId: 1 })
+    : +confirm(m));
+};
