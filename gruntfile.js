3e4e94b034639f098e8143c094049ccd8c644903
Gilgamesh Athoraya <e9gille@gmail.com>
2018-04-10 08:44:37 +0200

    linted gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,57 +1,55 @@
-const path     = require('path'),
-      electron = require('electron-connect').server.create(),
-      less_glob= require('less-plugin-glob');
+const path = require('path');
+const electron = require('electron-connect').server.create();
+const lessGlob = require('less-plugin-glob');
 
-module.exports=function(grunt){
+module.exports = function gruntTask(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-env');
 
-  grunt.registerTask('start','Start-Up electron server',function(){
+  grunt.registerTask('start','Start-Up electron server', () => {
     electron.start(['.','DEV_STYLE']);
   });
 
-  grunt.registerTask('restart','Restart RIDE and go into a session.',function(){
-    electron.broadcast('reboot',{message:'Shutdown then restart.'});
+  grunt.registerTask('restart', 'Restart RIDE and go into a session.', () => {
+    electron.broadcast('reboot', { message: 'Shutdown then restart.' });
   });
 
-  grunt.registerTask('reload-css','Attempt to live reload the CSS without having to restart/reload RIDE',function(){
-    electron.broadcast('css_update',{message:'Trigger styling reload.'});
+  grunt.registerTask('reload-css', 'Attempt to live reload the CSS without having to restart/reload RIDE', () => {
+    electron.broadcast('css_update', { message: 'Trigger styling reload.' });
   });
 
   grunt.initConfig({
-    pkg:grunt.file.readJSON('package.json'),
-    env:{
-      spawn:{
-        RIDE_SPAWN:'dyalog'
-      }
+    pkg: grunt.file.readJSON('package.json'),
+    env: {
+      spawn: { RIDE_SPAWN: 'dyalog' },
     },
-    less:{
-      development:{
-        options:{
-          paths:[path.join(__dirname,'style','less')],
-          plugins:[less_glob],
-          strictMath:true
+    less: {
+      development: {
+        options: {
+          paths: [path.join(__dirname, 'style', 'less')],
+          plugins: [lessGlob],
+          strictMath: true,
         },
-        files:{
-          './style/new-style.css':'./style/new-style.less'
-        }
-      }
-    },
-    watch:{
-      restart:{
-        files:['style/less/**/*.less','style/new-style.less'],
-        tasks:['less','env','restart'],
-        options:{spawn:false}
+        files: {
+          './style/new-style.css': './style/new-style.less',
+        },
       },
-      css_reload:{
-        files:['style/less/**/*.less','style/new-style.less'],
-        tasks:['less','reload-css'],
-        options:{spawn:false}
-      }
-    }
+    },
+    watch: {
+      restart: {
+        files: ['style/less/**/*.less', 'style/new-style.less'],
+        tasks: ['less', 'env', 'restart'],
+        options: { spawn: false },
+      },
+      css_reload: {
+        files: ['style/less/**/*.less', 'style/new-style.less'],
+        tasks: ['less', 'reload-css'],
+        options: { spawn: false },
+      },
+    },
   });
 
-  grunt.registerTask('default',['start','less','watch:css_reload']);
-  grunt.registerTask('spawn-reload',['env','start','less','watch:restart']);
-}
+  grunt.registerTask('default', ['start', 'less', 'watch:css_reload']);
+  grunt.registerTask('spawn-reload', ['env', 'start', 'less', 'watch:restart']);
+};
