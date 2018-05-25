b0b5f3950b8d085c520d5d6df04a492786a24104
Gilgamesh Athoraya <e9gille@gmail.com>
2018-05-25 14:20:29 +0200

    fix test for mac

--- test/ed.js
+++ test/ed.js
@@ -11,20 +11,23 @@ test(
     let text;
 
     await c.execute(() => { D.prf.prefixKey('<'); D.prf.ilf(0); D.prf.indent(-1); });
-    const eol = (await c.execute('D.win')) ? '\r\n' : '\n';
+    const [mac, win] = (await c.execute(() => [D.mac, D.win])).value;
+    const eol = win ? '\r\n' : '\n';
+    const cc = mac ? 'Meta' : 'Control';
+
     await c.keys([')ED <] ls', 'Enter']);
     await c.waitForExist('#ide .ride_win.edit_trace');
-    await c.keys(['A', 'Enter', 'A', 'Control', 'a', 'c', 'Control', 'Escape']);
+    await c.keys(['A', 'Enter', 'A', cc, 'a', 'c', cc, 'Escape']);
     text = await app.electron.clipboard.readText();
     t.is(text, `A${eol}A`);
 
     await c.waitForExist('#ide .ride_win.edit_trace', true);
     await c.waitForExist('#ide .ride_win');
+    await c.pause(100);
     await c.keys([')ED f', 'Enter']);
     await c.waitForExist('#ide .ride_win.edit_trace');
-    await c.keys(['Enter', '1', 'Control', 'a', 'c', 'Control', 'Escape']);
+    await c.keys(['Enter', '1', cc, 'a', 'c', cc, 'Escape']);
     text = await app.electron.clipboard.readText();
     t.is(text, `f${eol}1`);
-
   },
 );
