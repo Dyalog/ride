Dyalog/ride

commit fc2b2c4bf3f6115ac615264de681ca0af0034755

allow using ~ for home dir in cwd, fix #321

src/cn.js
@@ -12,8 +12,10 @@
   const rq = nodeRequire;
   const fs = rq('fs');
   const cp = rq('child_process');
+  const home = rq('os').homedir();
   const net = rq('net');
   const path = rq('path');
+  const untildify = x => (home ? x.replace(/^~(?=$|\/|\\)/, home) : x);
   const { esc } = D.util;
   const user = D.el ? process.env.USER : '';
   const MIN_V = [15, 0];
@@ -490,7 +492,7 @@
               if (x.args) args = args.concat(args, x.args.replace(/\n$/, '').split('\n'));
               try {
                 child = cp.spawn(x.exe, args, {
-                  cwd: x.cwd,
+                  cwd: untildify(x.cwd),
                   stdio,
                   detached: true,
                   env: $.extend(

commit 8791594fb23197ee6a9b71c0f03738f4f50f92a4

allow empty cwd fix issue introduced in fc2b2c4bf3f6115ac615264de681ca0af0034755

src/cn.js
@@ -15,7 +15,7 @@
   const home = rq('os').homedir();
   const net = rq('net');
   const path = rq('path');
-  const untildify = x => (home ? x.replace(/^~(?=$|\/|\\)/, home) : x);
+  const untildify = x => (x && home ? x.replace(/^~(?=$|\/|\\)/, home) : x);
   const { esc } = D.util;
   const user = D.el ? process.env.USER : '';
   const MIN_V = [15, 0];