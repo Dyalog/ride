436b6a289032f75da6d806255ade7c5323b3fb52
Gilgamesh Athoraya <e9gille@gmail.com>
2018-06-13 22:28:25 +0200

    toggle view stops with no open editors, fix #452

--- src/se.js
+++ src/se.js
@@ -507,6 +507,7 @@
     RC() { this.me.trigger('editor', 'cursorRight'); },
     SA() { this.me.trigger('editor', 'selectAll'); },
     TO() { this.me.trigger('editor', 'editor.fold'); }, // (editor.unfold) is there a toggle?
+    TVB() { D.prf.breakPts.toggle(); },
     TVO() { D.prf.fold.toggle(); },
     indentOrComplete(me) {
       const sels = me.getSelections();
