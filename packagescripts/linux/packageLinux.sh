001cf564d2a5b00a53e91b04f8ef793900be6679
Jason Rivers <jason@jasonrivers.co.uk>
2019-07-09 13:38:28 +0100

    disable removing .git folder

--- packagescripts/linux/packageLinux.sh
+++ packagescripts/linux/packageLinux.sh
@@ -237,7 +237,7 @@ for CPUTYPE in x64 armv7l ; do
 	mkdir -p "${SBOXDIR}/opt/ride-${BASE_VERSION}"
 	cp -R "${RIDEDIR}"/* "${SBOXDIR}/opt/ride-${BASE_VERSION}"/
 	find "${SBOXDIR}" -type f "(" -name "*.so" -o -name "*.svg" -o -name "*.js" ")" | xargs chmod -x # for lintian
-	find "${SBOXDIR}" -name ".git*" | xargs rm -r # for lintian
+	#find "${SBOXDIR}" -name ".git*" | xargs rm -r # for lintian
 	find "${SBOXDIR}" -type f "(" -name ".*" -o -name "*.c" ")" | xargs rm # for rpmlint
 	find "${SBOXDIR}" -xtype l | xargs rm -f # remove dangling symlinks, for rpmlint
 
