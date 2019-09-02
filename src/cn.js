edf2f07d41804948d96df46a2ca5bd3eaa56656e
Gilgamesh Athoraya <e9gille@gmail.com>
2019-09-02 17:16:29 +0200

    allow more messages when prompt = 0

--- src/cn.js
+++ src/cn.js
@@ -952,7 +952,7 @@
   module.exports = () => {
     D.send = (x, y) => {
       if (D.ide && !D.ide.promptType
-        && !/Interrupt$|TreeList|Reply|FormatCode/.test(x)) return;
+        && !/Interrupt$|TreeList|Reply|FormatCode|GetAutocomplete|SaveChanges|CloseWindow|Exit/.test(x)) return;
       sendEach([JSON.stringify([x, y])]);
     };
     const a = rq('electron').remote.process.argv;
