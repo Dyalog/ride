e3fb5848e22cbeb0ae2b1e1fd0c6cb3f9f2aef16
Gilgamesh Athoraya <e9gille@gmail.com>
2021-02-22 22:29:17 +0100

    Add 'local' variable to the preferences menu parser (#659)
    * Add 'local' variable to the preferences menu parser
    
    * Added ternary to filepath assignment to remove dev tools error
    
    * Moved x to local scope, tidied logic
    
    Co-authored-by: JonnersW <jonners.walton@btinternet.com>

--- src/km.js
+++ src/km.js
@@ -81,12 +81,13 @@
         return;
       }
       if (D.el && D.lastSpawnedExe) {
-        const [v] = D.el.dialog.showOpenDialogSync(D.elw, {
+        let x = D.el.dialog.showOpenDialogSync(D.elw, {
           title: 'Open file',
           filters: [],
           properties: ['openFile'],
-        });
-        if (!v) return;
+        })
+        if (!x) return;
+        const [v] = x
         if (/\.dws$/i.test(v)) {
           $.confirm(
             `Run Latent Expression of ${v.replace(/^.*[\\/]/, '')}?`,
