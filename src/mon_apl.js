b3fd0f6282e49347f32569e6187f6df90795b6f2
Karl Drejing <karl.drejing@tiamatica.com>
2022-08-18 09:32:44 +0200

    Autocompletion popup closes when backspacing, Fix #714 (#907)
    * trigger suggestions after period if available
    
    * added ac suggestion limit to prefs
    
    * removed double negation
    
    * trigger autocomplete after N characters
    
    * Use suggestWidgetController and hide when empty
    
    Co-authored-by: Gilgamesh Athoraya <e9gille@gmail.com>

--- src/mon_apl.js
+++ src/mon_apl.js
@@ -862,7 +862,10 @@
         /* eslint-enable no-template-curly-in-string */
         return { suggestions };
       }
-      if (D.send) {
+      const word = (((RegExp('⎕?[A-Z_a-zÀ-ÖØ-Ýß-öø-üþ∆⍙Ⓐ-Ⓩ0-9]*$').exec(s.slice(0, c)) || [])[0] || '')); // match left of cursor
+      const limit = D.prf.autoCompleteCharacterLimit();
+      if (D.send && (l[s] || ' ') === ' '
+        && (word.length >= limit || D.prf.autocompletion() === 'shell')) {
         D.send('GetAutocomplete', { line: s, pos: c - 1, token: model.winid });
         const m = model;
         return new Promise((complete, error, progress) => {
@@ -871,7 +874,7 @@
           };
         });
       }
-      return [];
+      return null;
     },
   });
   const aplHover = {
@@ -907,7 +910,7 @@
           };
         });
       }
-      return [];
+      return null;
     },
   };
   const aplFold = {
