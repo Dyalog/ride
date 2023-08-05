5a3126fc7f2f335e91856b36d897064ced68a238
Gilgamesh Athoraya <e9gille@gmail.com>
2023-08-05 16:27:49 +0200

    Refactor new session page

--- src/prf.js
+++ src/prf.js
@@ -19,6 +19,7 @@ D.prf = {};
   ['colourSchemes',      []],//objects describing user-defined colour schemes
   ['confirmations',      {}],//saved responses to TaskDialog questions
   ['connectOnQuit',      0], // open connection page when active session ends
+  ['defaultConfig',     ''], // default session configuration
   ['doubleClickToEdit',  1], // whether double clicking a function name in session or editor opens an editor on that function
   ['filenameInTitle',    1], // include filename in editor title
   ['floating',           0], //floating editor and tracer windows
